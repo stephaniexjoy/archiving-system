d13d35ddc4a3c2bd5c671d275c876b677f3557c7
stephaniexjoy <cristobalstephaniejoy@gmail.com>
2023-12-09 15:12:11 +0800

    Initial commit

---
+++ src/app/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div className='grid grid-rows-1 grid-col-2 gap-4 px-28'>
+            <div className="row-start-1 row-span-1 col-start-1 col-span-1 ml-10">
+                <a>
+                    <img src="../photos/ccs.png" alt="ccs" />
+                </a>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
