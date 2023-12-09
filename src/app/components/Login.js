354b3c7bb5e3e1b759624dba9e48e616fad2498a
stephaniexjoy <cristobalstephaniejoy@gmail.com>
2023-12-09 19:32:30 +0800

    initial commit

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -4,7 +4,7 @@ const Login = () => {
     return (
         <div className='grid grid-rows-1 grid-col-2 gap-4 px-28'>
             <div className="row-start-1 row-span-1 col-start-1 col-span-1 ml-10">
-                <a>
+                <a class="opacity-50">
                     <img src="../photos/ccs.png" alt="ccs" />
                 </a>
             </div>
--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Archiving System',
   description: '',
+  icon:'/favicon.ico'
 }
 
 export default function RootLayout({ children }) {
