c197f0d45bb855fab66ac63dedda044ff29a84fd
xcutiepatootie <mackycalabia@gmail.com>
2023-12-11 19:00:29 +0800

    new page and new component

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,23 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Archiving System',
+  description: '',
+  icon:'/favicon.ico'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <style>{inter.styles}</style>
+      </head>
+      <body className={`bg-[#5B0505] ${inter.className}`}>
+        {children}
+      </body>
+    </html>
+  )
+}
---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+"use client"
+import Login from './components/Login'
+
+export default function dashboard() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+    {/* Dashboard here */}
+     
+      
+      
+    </main>
+  )  
+}
