94d090a7589b4b37f17f548539f18239308b0f57
stephaniexjoy <cristobalstephaniejoy@gmail.com>
2023-12-13 23:38:38 +0800

    edit sidebar

--- src/app/components/Sidebar.js
+++ src/app/components/Sidebar.js
@@ -7,48 +7,31 @@ import { FaRegUser } from 'react-icons/fa';
 
 
 const Sidebar = () => {
-    
+
     return (
-        <aside className="bg-[#D9D9D9] text-black w-64 h-screen">
+        <aside className="absolute bg-[#D9D9D9] text-black w-64 h-[925px] top-0 left-0 z-40">
             <nav>
-                <div className="">
-                    
-                        <>
-                            <p><FaRegUser className='' />name</p>
-                            <p>email</p>
-                            <p>role</p>
-                        </>
-                   
+                <div className="h-10 text-center mt-4 py-2 text-base">
+                    <>
+                        <img class="h-20 w-20 rounded-full ml-24 mb-3" src="../photos/ccs.png" />
+                        {/* src="{person.imageUrl}" --> ito yung code dapat sa photo ginamit ko lang muna yung logo ng ccs */}
+                        <p>USER'S NAME</p>
+                    </>
                 </div>
-
-                <ul className="">
-                    
-                        <>
-                            <li className="mb-2 hover:bg-[#9D9494] ">
-                                <Link href="#" onClick={() => handleItemClick('Manage Account')}>Manage Accounts</Link>
-                            </li>
-                        </>
-                    
-
-                    <li className="mb-2 hover:bg-[#9D9494]" onClick={() => handleItemClick('Dashboard')}>
-                        <Link href="#">Home</Link>
+                <ul>
+                    <li className="mb-2 hover:bg-[#9D9494] h-10 text-center mt-24 py-2" onClick={() => handleItemClick('Dashboard')}>
+                        <Link href="#">HOME</Link>
                     </li>
-
-                   
-                        <>
-                            <li className="mb-2 hover:bg-[#9D9494]" onClick={() => handleItemClick('Profile')}>
-                                <Link href="#">Profile</Link>
-                            </li>
-                            <li className="mb-2 hover:bg-[#9D9494]" onClick={() => handleItemClick('Archiving')}>
-                                <Link href="#">Archiving</Link>
-                            </li>
-                        </>
-                   
+                    <li className="mb-2 hover:bg-[#9D9494] h-10 text-center mt-4 py-2" onClick={() => handleItemClick('Profile')}>
+                        <Link href="#">PROFILE</Link>
+                    </li>
+                    <li className="mb-2 hover:bg-[#9D9494] h-10 text-center mt-4 py-2" onClick={() => handleItemClick('Archiving')}>
+                        <Link href="#">ARCHIVING</Link>
+                    </li>
+                    <li className="mb-2 hover:bg-[#9D9494] h-10 text-center mt-4 py-2" onClick={() => signOut()}>
+                        <Link href="#">SIGN OUT</Link>
+                    </li>
                 </ul>
-
-                <li className="mb-2 hover:bg-[#9D9494]" onClick={() => signOut()}>
-                    <Link href="#">Sign Out</Link>
-                </li>
             </nav>
         </aside>
     )
