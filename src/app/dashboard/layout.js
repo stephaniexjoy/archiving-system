5c0dd9e20b4e47ac0e1718702865efe7c99ebb9e
xcutiepatootie <mackycalabia@gmail.com>
2023-12-14 13:20:52 +0800

    added sidebar in layout

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,14 @@
+import Sidebar from "../components/Sidebar";
+
+export default function DashboardLayout({
+  children // will be a page or nested layout
+}) {
+  return (
+    <section>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      <Sidebar />
+
+      {children}
+    </section>
+  );
+}
