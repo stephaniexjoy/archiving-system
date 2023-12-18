ec3c3884d6c5d00f82bd65c8e58ddc95db64abe8
kaye1015 <chzkcereneo15@gmail.com>
2023-12-18 09:20:17 +0800

    edited profile

--- src/app/dashboard/profile/page.js
+++ src/app/dashboard/profile/page.js
@@ -1,60 +1,141 @@
 "use client"
-
+import { FaLock } from "react-icons/fa";
 
 export default function profile() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
       <div>
         <div className="absolute top-[125px] left-[1px] bg-[#AD5606] [backdrop-filter:blur(4px)] w-[1800px] h-[1800px]" />
         <img
-          className="absolute top-[10px] left-[45%] w-[400px] h-[100px] object-cover"
+          className="absolute top-[10px] left-[755px] w-[400px] h-[100px] object-cover"
           alt=""
           src="../photos/E-Archiving System.png"
         />
-        <div className="absolute top-[550px] left-[25%] text-white  text-[31px] font-bold font-['Inter']">
-          Name:
+          <div className="absolute left-[250px] top-[140px] rounded-[50%] bg-[#DABB9C] shadow-[30px_0px_30px_rgba(0,_0,_0,_0.35)] w-[130px] h-[135px] [transform:_rotate(-0.3deg)] [transform-origin:0_0]" />
+          <div
+            className=" absolute left-[550px] top-[145px] text-[20px] 
+             text-white text-left w-[900px]
+            h-[30px]"
+          >
+            <div>Name:</div>
+            <div>Position:</div>
+            <div>Eligibility/Professional License:</div>
+            <div>School:</div>
+          </div>
+          <div
+            className=" absolute top-[175px] left-[1150px] text-[20px] 
+             text-white text-left w-[900px]
+            h-[30px]"
+          >
+            <div></div>
+            <div>Designation:</div>
+            <div>Specialization:</div>
+            <div>Certifications:</div>
+          </div>
+          <div
+            className=" absolute top-[285px] left-[1px] text-[20px] bg-[#5B0505]
+            [backdrop-filter:blur(4px)] text-white text-center w-[1800px]
+            h-[30px]"
+          >
+            ACADEMIC QUALIFICATIONS
+          </div>
+          <div
+            className=" absolute left-[200px] top-[325px] text-[20px] 
+             text-white text-left w-[900px]
+            h-[30px]"
+          >
+            <div>Baccalaureate Degree:</div>
+            <div>Masters Degree:</div>
+            <div>Doctorate Degree:</div>
+          </div>
+          <div
+            className=" absolute top-[325px] left-[970px] text-[20px] 
+             text-white text-left w-[900px]
+            h-[30px]"
+          >
+            <div>School:</div>
+            <div>School:</div>
+            <div>School:</div>
+          </div>
+          <div
+            className=" absolute top-[420px] left-[1px] text-[20px] bg-[#5B0505]
+            [backdrop-filter:blur(4px)] text-white text-center w-[870px]
+            h-[30px]"
+          >
+            SEMINARS AND TRAININGS ATTENDED
+          </div>
+          <div
+            className=" absolute top-[420px] left-[920px] text-[20px] bg-[#5B0505]
+            [backdrop-filter:blur(4px)] text-white text-center w-[1000px]
+            h-[30px]"
+          >
+            JOB EXPERIENCE
+          </div>
+          <div>
+            <h1
+              className="absolute top-[520px] left-[1px] text-[20px] bg-[#5B0505]
+            [backdrop-filter:blur(4px)] text-white text-center w-[1800px]
+            h-[30px]"
+            >
+              SUBJECTS HANDLED DESCRIPTIVE TITLE
+            </h1>
+          </div>
+          <div>
+            <h1
+              className="absolute top-[620px] left-[1px] text-[20px] bg-[#5B0505]
+            [backdrop-filter:blur(4px)] text-white text-center w-[1800px]
+            h-[30px]"
+            >
+              PAST DESIGNATIONS
+            </h1>
+          </div>
+          <div>
+            <h1
+              className="absolute top-[720px] left-[1px] text-[20px] bg-[#5B0505]
+            [backdrop-filter:blur(4px)] text-white text-center w-[1800px]
+            h-[30px]"
+            >
+              PRESENTED PAPERS
+            </h1>
+          </div>
+          <div>
+            <h1
+              className="absolute top-[820px] left-[1px] text-[20px] bg-[#5B0505]
+            [backdrop-filter:blur(4px)] text-white text-center w-[1800px]
+            h-[30px]"
+            >
+              EXTENSIONS PROJECTS
+            </h1>
+          </div>
         </div>
-        <div className="absolute top-[600px] left-[25%] text-white  text-[31px] font-bold font-['Inter']">
-          Age:
-        </div>
-        <div className="absolute top-[650px] left-[25%] text-white inline-block text-[31px] font-bold font-['Inter']">
-          Sex:
-        </div>
-        <div className="absolute top-[700px] left-[25%] text-white inline-block text-[31px] font-bold font-['Inter']">
-          Employee No.:
-        </div>
-        <div className="absolute top-[750px] left-[25%] text-white inline-block text-[31px] font-bold font-['Inter']">
-          Designation:
-        </div>
-        <div className="absolute top-[800px] left-[25%] text-white inline-block text-[31px] font-bold font-['Inter']">
-          Position:
-        </div>
-        <div className="absolute top-[850px] left-[25%] text-white inline-block text-[31px] font-bold font-['Inter']">
-          Plantilla Position:
-        </div>
-        <div className="absolute top-[900px] left-[25%] text-white inline-block text-[31px] font-bold font-['Inter']">
-          Institutional Email:
-        </div>
+
         <div className="flex justify-center">
           <button
             type="submit"
-            className="absolute top-[1000px] left-[50%] bg-[#D9D9D9] text-[20px] text-black text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
+            className="absolute top-[950px] left-[500px] bg-[#8F8F8F] text-[20px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
             // onClick={handleSubmit}
           >
-            EDIT PROFILE
+            EDIT
           </button>
         </div>
         <div className="flex justify-center">
           <button
             type="submit"
-            className="absolute top-[1050px] left-[50%] bg-[#D9D9D9] text-[20px] text-black text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
+            className="absolute top-[950px] left-[770px] bg-[#8F8F8F] text-[20px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
             //onClick={handleSubmit}
           >
-            EDIT PRIVACY
+            DOWNLOAD
           </button>
         </div>
-      </div>
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="absolute top-[950px] left-[1040px] bg-[#8F8F8F] text-[20px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
+            //onClick={handleSubmit}
+          >
+            PRINT
+          </button>
+        </div>
     </main>
   ); 
    
