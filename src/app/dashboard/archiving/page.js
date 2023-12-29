fc5ba8d2cd82b8444a31c1a1df092c6c30be8721
mapatriceee <matienzonoreenmapatrice@gmail.com>
2023-12-29 22:09:16 +0800

    edit archiving

--- src/app/dashboard/archiving/page.js
+++ src/app/dashboard/archiving/page.js
@@ -1,7 +1,7 @@
 "use client"
+
 import { FaSearch } from "react-icons/fa";
 import { FaUpload } from "react-icons/fa";
-
 export default function archiving() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -16,12 +16,12 @@ export default function archiving() {
              <h1 className="absolute top-[120px] left-[330px] inline-block text-[35px] opacity-60 font-semibold shadow-zinc-400">
                 FIND 
              </h1>
-            <input type="search" name='find' placeholder="" className="absolute top-[125px] left-[430px] text-3xl text-[#A89F9F] bg-[#D9D9D9] inline-block w-[800px] h-[37px] text-shadow-inner" />
+            <input type="search" name='find' placeholder="" className="absolute top-[125px] left-[430px] text-xl text-[#242323] bg-[#D9D9D9] inline-block w-[800px] h-[37px] text-shadow-inner" />
             
             <div className="absolute top-[125px] left-[1250px] bg-[#6A6A6A]  w-[40px] h-[40px] shadow-lg" />
                 <h1 ><FaSearch size='30' style={{ position: 'absolute', top: '130px', left: '1255px' }} /></h1>
             <div className="absolute top-[125px] left-[1300px] bg-[#6A6A6A]  w-[40px] h-[40px] shadow-lg cursor-pointer" />
-                <h1> <FaUpload size='30' style={{ position: 'absolute', top: '130px', left: '1305px' }} /></h1>
+                <h1> <FaUpload size='30' style={{ position: 'absolute', top: '130px', left: '1305px', cursor: 'pointer' }} /></h1>
             <div className="absolute top-[170px] left-[370px] inline-block text-[18px] text-orange-200 ">
                 MATERIAL 
             </div>
@@ -37,42 +37,79 @@ export default function archiving() {
             <div className="absolute top-[170px] left-[1250px] inline-block text-[18px] text-orange-200">
                 SORT BY
             </div>
-
+        
             <div className="dropdown">
             <div className="absolute top-[210px] left-[300px] bg-[#D9BA9C] backdrop-filter-blur-[4px] w-[220px] h-[50px] cursor-pointer ">
-                <select className="absolute top-[15px] left-[1px] bg-[#D9BA9C] text-black font-semibold w-[200px] cursor-pointer border-gray-300 rounded-md px-2 py-1" name="type" id="type">
+                <select className="absolute top-[15px] left-[1px] bg-[#D9BA9C]  text-black font-semibold w-[200px] cursor-pointer border-gray-300 rounded-md px-2 py-1 focus:outline-none" name="type" id="type">
+                <option value="select">--Select--</option>  
                 <option value="learning materials">Learning Materials</option>
                 <option value="training and seminar">Training and Seminar</option>
                 <option value="reserch conducted">Research Conducted</option>
                 <option value="extension conducted">Extension Conducted</option>
+                <option value="educational qualification">Educational Qualification</option>
+                <option value="faculty profile">Faculty Profile</option>
             </select>
+           
+            <div className="absolute top-[193px] left-[35px] bg-[#AD5606] backdrop-filter-blur-[4px] shadow-lg rounded-md w-[145px] h-[35px] "></div>
+             <div className="button"> 
+                <div className="absolute top-[200px] left-[40px] text-center outline-color rounded w-[130px]">
+                    <button>ADD CATEGORY</button>
+                </div>
+            </div>
+          
         </div>
-            <div className="absolute top-[210px] left-[545px] bg-[#D9BA9C] backdrop-filter-blur-[4px] w-[180px] h-[50px] cursor-pointer">
-                <select className="absolute top-[15px] left-[1px] bg-[#D9BA9C] text-black font-semibold w-[155px] cursor-pointer border-gray-300 rounded-md px-2 py-1" name="type" id="type">
+            <div className="absolute top-[210px] left-[540px] bg-[#D9BA9C] backdrop-filter-blur-[4px] w-[180px] h-[50px] cursor-pointer">
+                <select className="absolute top-[15px] left-[1px] bg-[#D9BA9C] text-black font-semibold w-[155px] cursor-pointer border-gray-300 rounded-md px-2 py-1 focus:outline-none" name="type" id="type">
+                <option value="select">--Select--</option>  
                 <option value="pdf">PDF</option>
                 <option value="ppt">PPT</option>
                 <option value="docx">DOCX</option>
             </select>
+           
+            <div className="absolute top-[193px] left-[20px] bg-[#AD5606] backdrop-filter-blur-[4px] shadow-lg rounded-md w-[145px] h-[35px] "></div>
+            <div className="button">
+                <div className="absolute top-[200px] left-[28px] text-center outline-color rounded shadow-lg w-[130px]">
+                    <button>ADD CATEGORY</button>
+                </div>
+            </div>
+           
         </div>
-            <div className="absolute top-[210px] left-[740px] bg-[#D9BA9C] backdrop-filter-blur-[4px] w-[200px] h-[50px] cursor-pointer">
-                <select className="absolute top-[15px] left-[1px] bg-[#D9BA9C] text-black font-semibold w-[175px] cursor-pointer border-gray-300 rounded-md px-2 py-1" name="program" id="program">
+            <div className="absolute top-[210px] left-[740px] bg-[#D9BA9C] backdrop-filter-blur-[4px] w-[210px] h-[50px] cursor-pointer">
+                <select className="absolute top-[15px] left-[1px] bg-[#D9BA9C] text-black font-semibold w-[185px] cursor-pointer border-gray-300 rounded-md px-2 py-1  focus:outline-none " name="program" id="program">
+                <option value="select">--Select--</option>  
                 <option value="Machine Learning">Machine Learning</option>
                 <option value="Programming Languages">Programming Languages</option>
                 <option value="Platform Technologie">Platform Technologies</option>
             </select>
+           
+            <div className="absolute top-[193px] left-[35px] bg-[#AD5606] backdrop-filter-blur-[4px] shadow-lg rounded-md w-[145px] h-[35px] "></div>
+            <div className="button">
+                <div className="absolute top-[200px] left-[40px] text-center outline-color rounded shadow-lg w-[130px]">
+                    <button>ADD CATEGORY</button>
+                </div>
+            </div>
+            
         </div>
             <div className="absolute top-[210px] left-[970px] bg-[#D9BA9C] backdrop-filter-blur-[4px] w-[200px] h-[50px] cursor-pointer">
-                <select className="absolute top-[15px] left-[13px] bg-[#D9BA9C] text-black font-semibold cursor-pointer border-gray-300 rounded-md px-2 py-1" name="instructor" id="instructor">
+                <select className="absolute top-[15px] left-[13px] bg-[#D9BA9C] text-black font-semibold cursor-pointer border-gray-300 rounded-md px-2 py-1 focus:outline-none  " name="instructor" id="instructor">
+                <option value="select">--Select--</option>  
                 <option value="Prince Andres">Prince Andres</option>
                 <option value="John Ryan Ual">John Ryan Ual</option>
                 <option value="Marco Del Rosario">Marco Del Rosario</option>
             </select>
+            
+            <div className="absolute top-[193px] left-[35px] bg-[#AD5606] backdrop-filter-blur-[4px] shadow-lg rounded-md w-[145px] h-[35px] "></div>
+            <div className="button">
+                <div className="absolute top-[200px] left-[40px] text-center outline-color rounded shadow-lg w-[130px]">
+                    <button>ADD CATEGORY</button>
+                </div>
+            </div>
         </div>
-
-            <div className="absolute top-[210px] left-[1190px] bg-[#D9BA9C] [backdrop-filter:blur(4px)] w-[210px] h-[50px] cursor-pointer" />
-        </div>         
+            <div className="absolute top-[210px] left-[1190px] bg-[#D9BA9C] [backdrop-filter:blur(4px)] w-[205px] h-[50px] ">
+            <input type = "date" className="absolute top-[13px] left-[10px] bg-[#D9BA9C] text-black font-semibold w-[180px] "/>
+        </div>
+    </div>
     </div>
 </main>
   ); 
-   
 }
