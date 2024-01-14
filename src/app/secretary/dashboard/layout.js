b2e160785cdd6c325ef328f39db8b81153ada630
xcutiepatootie <mackycalabia@gmail.com>
2024-01-14 17:44:18 +0800

    refactor: sidebar

---
+++ src/app/secretary/dashboard/layout.js
@@ -0,0 +1,24 @@
+import Sidebar from "../components/Sidebar";
+
+export default function DashboardLayout({
+  children // will be a page or nested layout
+}) {
+  return (
+
+    <>
+      {/* Include shared UI here e.g. a header or sidebar */}
+      < div className="flex" >
+
+        <div className="flex">
+          <Sidebar />
+        </div>
+
+        <div className="flex px-6 ">
+          {children}
+        </div>
+
+      </div >
+
+    </>
+  );
+}
