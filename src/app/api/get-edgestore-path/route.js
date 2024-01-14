d0e7f301ae8ccfdd59528972bef153a5943e10f4
xcutiepatootie <mackycalabia@gmail.com>
2024-01-14 16:48:52 +0800

    refactor: es

--- src/app/api/get-edgestore-path/route.js
+++ src/app/api/get-edgestore-path/route.js
@@ -2,11 +2,11 @@ import { backendClient } from "@/app/lib/edgestore-server";
 export async function GET(req) {
 
     try {
-        const res = await backendClient.publicFiles.listFiles();
+        const res = await backendClient.publicFiles1.listFiles();
 
 
         console.log(res)
-        return new Response(JSON.stringify(res), { status: 200, statusText: "User Successfully Created" })
+        return new Response(JSON.stringify(res), { status: 200, statusText: "Data Successfully Fetched" })
     } catch (err) {
         return new Response('Error', { status: 500, statusText: err })
     }
