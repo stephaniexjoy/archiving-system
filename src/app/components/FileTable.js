d06f3f82b1083fc8f919ceaf78df432f711faf4b
xcutiepatootie <mackycalabia@gmail.com>
2024-01-16 14:58:29 +0800

    refactor:

--- src/app/components/FileTable.js
+++ src/app/components/FileTable.js
@@ -6,11 +6,11 @@ async function getData() {
     const res = await fetch('http://localhost:3000/api/get-file-path')
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
-
+/* 
     if (!res.ok) {
         // This will activate the closest `error.js` Error Boundary
         throw new Error('Failed to fetch data')
-    }
+    } */
 
     return res.json()
 }
