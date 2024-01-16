ac9b031659f4c7aa9fd699fe09ace0eec5b5871e
kaye1015 <chzkcereneo@gmail.com>
2024-01-17 01:01:58 +0800

    edited profile

--- src/app/dashboard/profile/page.js
+++ src/app/dashboard/profile/page.js
@@ -6,7 +6,7 @@ export default function profile() {
   return (
     <div className="flex flex-auto w-auto h-screen">
       
-      <div className="flex flex-col mt-5 md:mr-10 lg:mr-20 items-center h-screen">
+      <div className="flex flex-col w-full h-screen items-center">
       <Image
           className=" top-[10px] left-[670px] mr-20  w-[400px] h-[100px] object-cover"
           alt=""
@@ -14,18 +14,22 @@ export default function profile() {
           height={400}
           src="/photos/E-Archiving System.png"
         />
-        <h1 className="flex flex-row-reverse"><FaUserLock size="40" style={{ top: "40px", left: "1660px"}} /></h1>
 
-        <div className=" top-[125px] left-[1px] bg-[#AD5606] [backdrop-filter:blur(4px)] w-screen h-screen mt-10">
+        <div>
+          <FaUserLock size="40" style={{ color: "white", top: "40px", left: "1660px"}} />
+        </div>
+
+        <div className=" border bg-[#AD5606] w-auto md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl mx-auto h-full mt-10 p-8 rounded-lg shadow-lg">
+      
         <div className="flex flex-row gap-x-10 justify-center ">
-            <div className="mt-10 ml-20 basis-1/4 left-[10px] object-center" >
+            <div className="mt-8 ml-20 basis-1/5 left-[10px] object-center" >
             <img
                 className="rounded-[50%]  w-[130px] h-[135px] object-cover  mb-5"
                 alt=""
                 src="/profile.jpg"
                />
             </div>
-            <div className="basis-1/4 text-[20px] text-white text-left w-[900px] h-[30px]">
+            <div className="basis-2/5 text-[20px] text-white text-left w-[900px] h-[30px]">
             
               <div className="mt-8">Name:</div>
               <div >Position:</div>
@@ -34,27 +38,26 @@ export default function profile() {
            
             </div>
     
-            <div className="mt-10 ml-5 basis-1/3 text-[20px] left-[1150px]
+            <div className="mt-10 ml-5 basis-2/5 text-[20px] left-[1150px]
              text-white text-left w-[900px] h-[30px]">
-            <div className=""></div>
                 <div className=" mt-5 top-[350px]">Designation:</div>
                 <div >Specialization:</div>
                 <div >Certifications:</div>
             </div>
           </div>
           <div
-          className="top-[285px] left-[1px] text-[20px] bg-[#5B0505] [backdrop-filter:blur(4px)] text-white text-center w-[1800px]h-[30px]">
+          className="top-[285px] left-[1px] text-[20px] bg-[#5B0505] [backdrop-filter:blur(4px)] text-white text-center w-[1800px] h-[30px]">
           ACADEMIC QUALIFICATIONS
         </div>
-        <div className="mr-5">
+        <div className="">
         <div className="flex flex-row gap-x-10 justify-center">
-          <div className="my-5 left-[200px] top-[325px] text-[20px] text-white text-left w-[900px] h-[30px]">
+          <div className="ml-12 my-5 basis-1/2 top-[325px] text-[20px] text-white text-left w-[900px] h-[30px]">
             <div className="">Baccalaureate Degree:</div>
-            <div>Masters Degree:</div>
+            <div>Master's Degree:</div>
             <div>Doctorate Degree:</div>
           </div>
 
-            <div className=" my-5 basis-1/4 top-[325px] left-[870px] text-[20px] text-white text-left w-[900px] h-[30px">
+            <div className=" my-5 basis-1/2 top-[325px] left-[870px] text-[20px] text-white text-left w-[900px] h-[30px">
               
               <div>School:</div>
               <div>School:</div>
@@ -72,7 +75,7 @@ export default function profile() {
         </div>
         <div
           className=" basis-1/2 top-[420px] left-[1500px] text-[20px] bg-[#5B0505]
-            [backdrop-filter:blur(4px)] text-white text-center w-[1000px]
+            [backdrop-filter:blur(4px)] text-white text-center w-auto
             h-[30px]"
         >
           JOB EXPERIENCE
