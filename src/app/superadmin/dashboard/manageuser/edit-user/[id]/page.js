74d53c78a676476eb6b742e14ab9602a662379ef
xcutiepatootie <mackycalabia@gmail.com>
2024-01-17 11:54:00 +0800

    feat: functions of view and edit user

--- src/app/superadmin/dashboard/manageuser/edit-user/[id]/page.js
+++ src/app/superadmin/dashboard/manageuser/edit-user/[id]/page.js
@@ -4,6 +4,8 @@ import { FaLock } from "react-icons/fa";
 import { IoMdEyeOff } from "react-icons/io";
 import { HiPencilAlt } from "react-icons/hi";
 import { db } from "@/app/lib/prisma_db";
+import bcrypt from 'bcrypt'
+
 
 const page = async ({ params }) => {
 
@@ -12,7 +14,30 @@ const page = async ({ params }) => {
   })
   console.log(user)
 
+  const updateUser = async (formData) => {
+    "use server"
 
+    const positionUpd = formData.get("position")
+    const emailUpd = formData.get("email")
+    const passwordUpd = formData.get("password")
+    const passConfirm = formData.get("passwordconfirm")
+
+    const passUpdHashed = await bcrypt.hash(passwordUpd, 12)
+
+    if (passwordUpd === passConfirm) {
+      const updUser = await db.user.update({
+        where: { id: user.id },
+        data: {
+          // Update the fields you want to modify
+          position: positionUpd,
+          email: emailUpd,
+          password: passUpdHashed
+          // Add other fields as needed
+        },
+      })
+    }
+
+  }
 
 
 
@@ -32,104 +57,109 @@ const page = async ({ params }) => {
           <h1 className=" text-[50px] mt-2 my-12 font-semibold  text-orange-900 text-center ">
             {user.name} {params.id}
           </h1>
-          <div className="flex justify-center">
-            <div className=" bg-white w-[600px] h-[77px] md:h-20 ">
-              <select
-                className="mt-3 ml-36 text-3xl 
-                  text-[#A89F9F] inline-block w-[400px] justify-around text-shadow-inner cursor-pointer"
-                name="type"
-                id="position"
+          <form action={updateUser}>
+            <div className="flex justify-center">
+
+              <div className=" bg-white w-[600px] h-[77px] md:h-20 ">
+                <select
+                  className="mt-3 ml-36 text-3xl 
+                text-[#A89F9F] inline-block w-[400px] justify-around text-shadow-inner cursor-pointer"
+                  name="position"
+                  id="position"
+                >
+                  <option value={user.position}>{user.position}</option>
+                  <option value="Associate Dean">Associate Dean</option>
+                  <option value="Secretary">Secretary</option>
+                  <option value="Faculty">Faculty</option>
+                  <option value="Unit Head">Unit Head</option>
+                  <option value="Coordinator">Coordinator</option>
+                </select>
+                <h1>
+                  <HiPencilAlt
+                    className=" -mt-14 ml-6"
+                    size="55"
+                    color="#000000"
+                    opacity="26%"
+                  />
+                </h1>
+              </div>
+            </div>
+            <div className="mt-4 flex justify-center">
+              <div className=" bg-white w-[600px] h-[77px] md:h-20">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder={`${user.email}`}
+                  className="mt-6 ml-36 text-3xl 
+                text-[#A89F9F] inline-block w-[400px] h-[37px] text-shadow-inner"
+                />
+                <h1>
+                  <FaUserAlt
+                    className="-mt-10 ml-6"
+                    size="50"
+                    color="#000000"
+                    opacity="26%"
+                    style={{ top: "675px", left: "870px" }}
+                  />
+                </h1>
+              </div>
+            </div>
+            <div className="mt-4 flex justify-center">
+              <div className=" bg-white w-[600px] h-[77px] md:h-20">
+                <input
+                  type="password"
+                  name="password"
+
+                  placeholder=" PASSWORD"
+                  className="mt-6 ml-36 text-3xl 
+                text-[#A89F9F] inline-block justify-around  w-[400px] h-[37px] text-shadow-inner"
+                />
+                <h1>
+                  <FaLock
+                    className="-mt-10 ml-6"
+                    size="50"
+                    color="#000000"
+                    opacity="26%"
+                  />
+                </h1>
+              </div>
+            </div>
+            <div className="mt-4 flex justify-center">
+              <div className=" bg-white w-[600px] h-[77px] md:h-20">
+                <input
+                  type="password"
+                  name="passwordconfirm"
+                  placeholder="CONFIRM PASSWORD"
+                  className="mt-6 ml-36 text-3xl 
+                text-[#A89F9F]  w-[400px] h-[37px] text-shadow-inner"
+                />
+                <h1>
+                  <FaLock
+                    className="-mt-10 ml-6"
+                    size="50"
+                    color="#000000"
+                    opacity="26%"
+                  />
+                </h1>
+              </div>
+            </div>
+            <div className=" mt-10 flex justify-center  ">
+              <button
+                type="submit"
+                className=" bg-[#737373] text-[20px] font-bold text-white text-center inline-block w-[247px] h-[38px]  hover:bg-gray-400"
               >
-                <option value={user.position}>{user.position}</option>
-                <option value="Associate Dean">Associate Dean</option>
-                <option value="Secretary">Secretary</option>
-                <option value="Faculty">Faculty</option>
-                <option value="Unit Head">Unit Head</option>
-                <option value="Coordinator">Coordinator</option>
-              </select>
-              <h1>
-                <HiPencilAlt
-                  className=" -mt-14 ml-6"
-                  size="55"
-                  color="#000000"
-                  opacity="26%"
-                />
-              </h1>
+                CONFIRM
+              </button>
+
+              <button
+                type="confirm"
+                className="ml-10 bg-[#737373] text-[20px] font-bold text-white text-center inline-block w-[247px] h-[38px]  hover:bg-gray-400"
+              >
+                BACK
+              </button>
             </div>
-          </div>
-          <div className="mt-4 flex justify-center">
-            <div className=" bg-white w-[600px] h-[77px] md:h-20">
-              <input
-                type="email"
-                name="email"
-                placeholder={`${user.email}`}
-                className="mt-6 ml-36 text-3xl 
-                    text-[#A89F9F] inline-block w-[400px] h-[37px] text-shadow-inner"
-              />
-              <h1>
-                <FaUserAlt
-                  className="-mt-10 ml-6"
-                  size="50"
-                  color="#000000"
-                  opacity="26%"
-                  style={{ top: "675px", left: "870px" }}
-                />
-              </h1>
-            </div>
-          </div>
-          <div className="mt-4 flex justify-center">
-            <div className=" bg-white w-[600px] h-[77px] md:h-20">
-              <input
-                type="password"
-                name="password"
-             
-                placeholder=" PASSWORD"
-                className="mt-6 ml-36 text-3xl 
-                    text-[#A89F9F] inline-block justify-around  w-[400px] h-[37px] text-shadow-inner"
-              />
-              <h1>
-                <FaLock
-                  className="-mt-10 ml-6"
-                  size="50"
-                  color="#000000"
-                  opacity="26%"
-                />
-              </h1>
-            </div>
-          </div>
-          <div className="mt-4 flex justify-center">
-            <div className=" bg-white w-[600px] h-[77px] md:h-20">
-              <input
-                type="confirmpassword"
-                name="password"
-                placeholder="CONFIRM PASSWORD"
-                className="mt-6 ml-36 text-3xl 
-                    text-[#A89F9F]  w-[400px] h-[37px] text-shadow-inner"
-              />
-              <h1>
-                <FaLock
-                  className="-mt-10 ml-6"
-                  size="50"
-                  color="#000000"
-                  opacity="26%"
-                />
-              </h1>
-            </div>
-          </div>
-          <div className=" mt-10 flex justify-center  ">
-            <button
-              type="confirm"
-              className=" bg-[#737373] text-[20px] font-bold text-white text-center inline-block w-[247px] h-[38px]  hover:bg-gray-400"
-            >
-              CONFIRM
-            </button>
-            <button
-              type="confirm"
-              className="ml-10 bg-[#737373] text-[20px] font-bold text-white text-center inline-block w-[247px] h-[38px]  hover:bg-gray-400"
-            >
-              BACK
-            </button>
-          </div>
+          </form>
+
         </div>
       </div>
     </div >
--- src/app/superadmin/dashboard/manageuser/view-user/[id]/page.js
+++ src/app/superadmin/dashboard/manageuser/view-user/[id]/page.js
@@ -1,58 +1,66 @@
 import React from 'react'
 import Image from 'next/image'
+import { db } from '@/app/lib/prisma_db'
 
-const page = () => {
+
+const page = async ({ params }) => {
+
+    const user = await db.user.findUnique({
+        where: { id: parseInt(params.id) }
+    })
+    console.log(user)
+
     return (
         <div>
             <div className="flex flex-col w-full items-center">
                 <Image
-                src="/photos/E-Archiving System.png"
-                alt="E-Archiving System"
-                width={570}
-                height={150}
-                objectFit="cover"
+                    src="/photos/E-Archiving System.png"
+                    alt="E-Archiving System"
+                    width={570}
+                    height={150}
+                    objectFit="cover"
                 />
-            <div className="bg-[#DABB9C] w-full h-auto bg-cover flex">
-                <div className="flex ml-[100px] mt-7 mb-7">
-                    <Image src="/profile.jpg" alt="Image" className="max-w-[170px] h-[170px] object-fit-fill rounded-full" width={170} height={170}/>
-                    
-                    <div className="ml-16 text-[#5B0505] text-[25px] font-bold text-left">
-                        <div className="grid grid-cols-2 gap-x-96">
-                            <div>
-                                <div>Name:</div>
-                            </div>
-                            <div>
-                                <div></div>
-                            </div>
-                            <div>
-                                <div>Position:</div>
-                            </div>
-                            <div>
-                                <div>Designation:</div>
-                            </div>
-                            <div>
-                                <div>Eligibility/Professional License:</div>
-                            </div>
-                            <div>
-                                <div>Specialization:</div>
-                            </div>
-                            <div>
-                                <div>School:</div>
-                            </div>
-                            <div>
-                                <div>Certifications:</div>
+                <div className="bg-[#DABB9C] w-full h-auto bg-cover flex">
+                    <div className="flex ml-[100px] mt-7 mb-7">
+                        <Image src="/profile.jpg" alt="Image" className="max-w-[170px] h-[170px] object-fit-fill rounded-full" width={170} height={170} />
+
+                        <div className="ml-16 text-[#5B0505] text-[25px] font-bold text-left">
+                            <div className="grid grid-cols-2 gap-x-96">
+                                <div>
+                                    <div>Name: {user.name}</div>
+                                </div>
+                                <div>
+                                    <div></div>
+                                </div>
+                                <div>
+                                    <div>Position: {user.position}</div>
+                                </div>
+                                <div>
+                                    <div>Designation: {user.designation}</div>
+                                </div>
+                                <div>
+                                    <div>Eligibility/Professional License: {user.license} </div>
+                                </div>
+                                <div>
+                                    <div>Specialization: {user.specialization}</div>
+                                </div>
+                                <div>
+                                    <div>School:</div>
+                                </div>
+                                <div>
+                                    <div>Certifications:</div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
-                ACADEMIC QUALIFICATIONS
-            </div>
+                <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
+                    ACADEMIC QUALIFICATIONS
+                </div>
 
-            <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
-                <div className="ml-32 text-[#5B0505] text-[25px] font-bold text-left">
+                <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
+                    <div className="ml-32 text-[#5B0505] text-[25px] font-bold text-left">
                         <div className="grid grid-cols-2 gap-x-96">
                             <div>
                                 <div>Baccalaureate Degree:</div>
@@ -73,80 +81,80 @@ const page = () => {
                                 <div>School:</div>
                             </div>
                         </div>
+                    </div>
                 </div>
-            </div>
 
-            <div className="flex flex-column items-center w-full">
-                <div className="text-[30px] bg-[#5B0505] backdrop-filter-blur-[4px] font-[Century Gothic] text-white text-center h-[50px] w-[49%] font-bold">
+                <div className="flex flex-column items-center w-full">
+                    <div className="text-[30px] bg-[#5B0505] backdrop-filter-blur-[4px] font-[Century Gothic] text-white text-center h-[50px] w-[49%] font-bold">
                         SEMINARS AND TRAININGS ATTENDED
-                </div>
-                <div className="bg-[#DABB9C] text-center h-[50px] w-[2%]">
-                        
-                </div>
-                <div className="text-[30px] bg-[#5B0505] backdrop-filter-blur-[4px] font-[Century Gothic] text-white text-center h-[50px] w-[49%] font-bold">
+                    </div>
+                    <div className="bg-[#DABB9C] text-center h-[50px] w-[2%]">
+
+                    </div>
+                    <div className="text-[30px] bg-[#5B0505] backdrop-filter-blur-[4px] font-[Century Gothic] text-white text-center h-[50px] w-[49%] font-bold">
                         JOB EXPERIENCE
-                </div>
-            </div>
-
-            <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
-                <div className="h-32">
-                </div>
-            </div>
-
-            <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
-                SUBJECTS HANDLED (DESCRIPTIVE TITLE)
-            </div>
-
-            <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
-                <div className="h-32">
-                </div>
-            </div>
-
-            <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
-                PAST DESIGNATIONS
-            </div>
-
-            <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
-                <div className="h-32">
-                </div>
-            </div>
-
-            <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
-                PRESENTED PAPERS
-            </div>
-
-            <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
-                <div className="h-32">
-                </div>
-            </div>
-
-            
-            <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
-                EXTENSION PROJECTS
-            </div>
-
-            <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
-                <div className="h-32">
+                    </div>
                 </div>
 
-                <div className="flex justify-center">
-                    <button type="submit" className="bg-[#8F8F8F] text-[30px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)] ml-20 mt-16">
-                        BACK
-                    </button>
+                <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
+                    <div className="h-32">
+                    </div>
                 </div>
-                <div className="flex justify-center">
-                    <button type="submit" className="bg-[#8F8F8F] text-[30px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)] ml-[1000px] mt-16">
-                        DOWNLOAD
-                    </button>
+
+                <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
+                    SUBJECTS HANDLED (DESCRIPTIVE TITLE)
                 </div>
-                <div className="flex justify-center">
-                    <button type="submit" className="bg-[#8F8F8F] text-[30px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)] ml-8 mt-16">
-                        PRINT
-                    </button>
+
+                <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
+                    <div className="h-32">
+                    </div>
+                </div>
+
+                <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
+                    PAST DESIGNATIONS
+                </div>
+
+                <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
+                    <div className="h-32">
+                    </div>
+                </div>
+
+                <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
+                    PRESENTED PAPERS
+                </div>
+
+                <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
+                    <div className="h-32">
+                    </div>
+                </div>
+
+
+                <div className=" text-[30px] bg-[#5B0505] backdrop-filter:blur(4px)] font-[Century Gothic] text-white text-center h-[50px] w-full font-bold">
+                    EXTENSION PROJECTS
+                </div>
+
+                <div className="bg-[#DABB9C] w-screen h-auto bg-cover flex">
+                    <div className="h-32">
+                    </div>
+
+                    <div className="flex justify-center">
+                        <button type="submit" className="bg-[#8F8F8F] text-[30px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)] ml-20 mt-16">
+                            BACK
+                        </button>
+                    </div>
+                    <div className="flex justify-center">
+                        <button type="submit" className="bg-[#8F8F8F] text-[30px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)] ml-[1000px] mt-16">
+                            DOWNLOAD
+                        </button>
+                    </div>
+                    <div className="flex justify-center">
+                        <button type="submit" className="bg-[#8F8F8F] text-[30px] text-white text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)] ml-8 mt-16">
+                            PRINT
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
 
