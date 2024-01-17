f4b208eb5fa4fa314aead470c427af9a868eb6c9
xcutiepatootie <mackycalabia@gmail.com>
2024-01-17 14:17:46 +0800

    feat: list of activity base from the database

--- src/app/superadmin/dashboard/page.js
+++ src/app/superadmin/dashboard/page.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { db } from '@/app/lib/prisma_db'
+import { revalidatePath } from 'next/cache'
 
 
-function page() {
+async function page() {
+
+  const fileCount = await db.file.count()
+  const userCount = await db.user.count()
+  const secCount = await db.user.count({ where: { position: "Secretary" } })
+
+  console.log(fileCount)
+  console.log(userCount)
+
+
+  const activities = await db.activity.findMany()
+  console.log(activities)
+
+  const activitiesFormatted = activities.map(activity => ({
+    ...activity,
+    createdAt: new Date(activity.createdAt).toLocaleString(),
+  }));
+
   return (
 
     <div className="flex flex-col w-auto h-screen">
@@ -24,7 +43,7 @@ function page() {
           <div className='mt-3 flex grid-cols-3 space-x-40 place-content-stretch justify-center h-16 md:h-20 font-bold font-[Times New Roman]'>
             <div className='text-center'>
               <p className='mt-11 text-8xl text-[#F1B177] '>
-                36
+                {fileCount}
               </p>
               <p className='mt-5 text-2xl'>
                 ARCHIVED MATERIALS
@@ -32,7 +51,7 @@ function page() {
             </div>
             <div className='text-center'>
               <p className=' mt-11 text-8xl text-[#F1B177]'>
-                19
+                {userCount}
               </p>
               <p className='mt-5 text-2xl'>
                 NO. OF FACULTY MEMBERS
@@ -40,7 +59,7 @@ function page() {
             </div>
             <div className='text-center'>
               <p className=' mt-11 text-8xl text-[#F1B177]'>
-                1
+                {secCount}
               </p>
               <p className='mt-5 text-2xl text-center'>
                 NO. OF SECRETARY
@@ -59,28 +78,21 @@ function page() {
                 </tr>
               </thead>
               <tbody className='bg-[#D9D9D9] text-black text-xl'>
-                <tr>
-                  <td className='py-2'>PRINCE ANDRES</td>
-                  <td>PART-TIME INSTRUCTOR</td>
-                  <td>UPLOADED A FILE</td>
-                  <td>December 13, 2023 (9:45)</td>
-                </tr>
-                <tr>
-                  <td className='py-2'>AL JOHN VILLAREAL</td>
-                  <td>REGULAR INSTRUCTOR</td>
-                  <td>DELETED A FILE</td>
-                  <td>December 10, 2023 (16:30)</td>
-                </tr>
-                <tr>
-                  <td className='py-2'>CATHERINE CASTILLO</td>
-                  <td>REGULAR INSTRUCTOR</td>
-                  <td>EDITED A FILE</td>
-                  <td>December 7, 2023 (7:30)</td>
-                </tr>
+
+                {/*Map The activities*/}
+                {activitiesFormatted.map((activity) => (
+                  <tr key={activity.id}>
+                    <td className='py-2'>{activity.name}</td>
+                    <td>{activity.position}</td>
+                    <td>{activity.type}</td>
+                    <td>{activity.createdAt}</td>
+                  </tr>
+                ))}
+
               </tbody>
             </table>
           </div>
-          
+
         </div>
       </div>
     </div>
