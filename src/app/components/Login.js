25dc236e61e4aedd21d2af7f53f201d1f0f8c59b
Asdf123ah <0320-0770@lspu.edu.ph>
2024-01-26 21:25:28 +0800

    Edit new login

--- src/app/components/Login.js
+++ src/app/components/Login.js
@@ -39,59 +39,47 @@ const Login = () => {
     return (
         <div className='flex flex-auto flex-col columns-1 place-items-center mx-0'>
             <img
-                className="-mt-7 ml-96 mb-10 w-[918px] h-[250px] object-cover"
-                alt=""
-                src="../photos/E-Archiving System.png"
+                className="absolute top-[0] left-[0] w-[30%] h-[100%] object-cover" alt="CCS Logo Half" src="../photos/ccsLogoHalf.png" style={{ opacity: '100%' }}
             />
-            <img
-                className="absolute top-[0] left-[-10px] w-[1100px] h-[1100px] object-cover"
-                alt=""
-                src="../photos/ccs.png"
-                style={{ marginLeft: "-550px", opacity: '80%' }}
-            />
-            <div className="absolute -top-2 left-[-10px] w-[1110px] h-[1110px] rounded-[50%] bg-[#DABB9C] shadow-[30px_0px_30px_rgba(0,_0,_0,_0.35)]"
-                style={{ marginLeft: "-550px", opacity: '70%' }} />
+            <div className="flex flex-col items-center justify-center bg-white w-screen h-[74vh] mt-[7%]">
+                <div className="bg-white items-center p-0 rounded-3xl w-[50%] h-[120%] ml-[26%] shadow-[0_0_15px_15px_rgba(0,_0,_0,_0.65)]">
+                    <h1 className="ml-[38%] mt-[8%] text-black text-4xl">WELCOME TO</h1>
+                    <img
+                        className="mt-0 ml-[10%] mb-[5%] w-[85%] h-[20%] object-cover" alt="E-Archiving System Maroon" src="../photos/E-Archiving System Maroon.png"
+                    />
+                    <img
+                        className="w-[100%] h-[8%] object-cover" alt="Login Logo Orange" src="../photos/loginLogoOrange.jpg"
+                    />
+                    <div className="ml-[18%] mb-[5%] mt-[7%] bg-white w-[65%] h-[77px] md:md:h-20 border border-black">
+                        <div className='flex'>
+                            <h1 ><FaUserAlt className="mt-3 ml-5" size='50' color='#000000' opacity='26%' /></h1>
+                            <input type="text" name='email' placeholder="USERNAME" className="mt-5 ml-8 text-3xl border-none
+                        text-black inline-block w-[80%] h-[37px] text-shadow-inner"
+                        onChange={(e) => { setEmail(e.target.value) }} />
+                        </div>
+                    </div>
 
-            <div className="flex flex-auto flex-col columns-1 bg-[#AD5606] w-screen h-[600px] place-items-center">
-                <div className="ml-96 mb-20 mt-20 bg-white w-[600px] h-[77px] md:md:h-20">
-                    <div className='flex'>
-                        <h1 ><FaUserAlt className="mt-3 ml-5" size='50' color='#000000' opacity='26%' /></h1>
-                        <input type="text" name='email' placeholder="USERNAME" className="mt-5 ml-8 text-3xl border-none
-                    text-black inline-block w-[450px] h-[37px] text-shadow-inner"
-                    onChange={(e) => { setEmail(e.target.value) }} />
+                    <div className="ml-[18%] bg-white w-[65%] h-[77px] md:h-20 border border-black">
+                        <div className='mt-4 flex'>
+                            <h1 ><FaLock className="mt-0 ml-5" size='50' color='#000000' opacity='26%' style={{ top: '675px', left: '870px' }} /></h1>
+                            <input type="password" name='password' placeholder="PASSWORD" className="mt-2 ml-8 text-3xl border-none text-black inline-block w-[80%] h-[37px] text-shadow-inner"
+                        onChange={(e) => { setPassword(e.target.value) }} />
+                        </div>
+                    </div>
+
+                    <div className="flex justify-end">
+                        <b className="mr-[15%] text-[#5B0505]">
+                            <ForgotPassword />
+                        </b>
+                    </div>
+
+                    <div className='flex justify-center'>
+                        <button type='submit' className="mt-[5%] mb-10 bg-[#5B0505] rounded-3xl text-[20px] font-bold text-white text-center inline-block w-[247px] h-[43px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
+                            onClick={handleSubmit}>LOGIN</button>
                     </div>
                 </div>
-
-                <div className="ml-96 bg-white w-[600px] h-[77px] md:h-20">
-                    <div className='mt-4 flex'>
-                        <h1 ><FaLock className="mt-0 ml-5" size='50' color='#000000' opacity='26%' style={{ top: '675px', left: '870px' }} /></h1>
-                        <input type="password" name='password' placeholder="PASSWORD" className="mt-2 ml-8 text-3xl border-none text-black inline-block w-[450px] h-[37px] text-shadow-inner"
-                    onChange={(e) => { setPassword(e.target.value) }} />
-                    </div>
-                </div>
-
-                <b>
-                    <ForgotPassword />
-                </b>
-                <div className='flex justify-center'>
-                    <button type='submit' className="ml-96 mt-4 bg-[#D9D9D9] text-[20px] font-bold text-black text-center inline-block w-[247px] h-[38px] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
-                        onClick={handleSubmit}>LOGIN</button>
-                </div>
-
             </div>
-
-
-            <div
-                className=" top-[797px] left-[921px] bg-gainsboro w-[247px] h-12 cursor-pointer"
-
-            />
-
-
-
-
-
         </div>
-
     )
 }
 
