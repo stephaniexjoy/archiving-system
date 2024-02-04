3e04ebd0b5b91344e2196726024327285c57f4fb
xcutiepatootie <mackycalabia@gmail.com>
2024-02-04 15:51:36 +0800

    feat: added function to fill user_education table

--- src/app/api/users/create-user/route.js
+++ src/app/api/users/create-user/route.js
@@ -37,6 +37,9 @@ export async function POST(req) {
                 age,
                 sex,
                 employee_no,
+                education: {
+                    create: {}
+                }
             }
         })
 
