8f8de832709a86c794aca92b2711d461aaf79e30
xcutiepatootie <mackycalabia@gmail.com>
2024-02-06 12:12:56 +0800

    feat: archiving secretary tabs

--- src/app/components/New_Components/Secretary/Archiving/SecretaryArchivingTab.js
+++ src/app/components/New_Components/Secretary/Archiving/SecretaryArchivingTab.js
@@ -17,8 +17,12 @@ import {
 } from "@/components/ui/tabs"
 
 import React from 'react'
+import Files_Archiving_tabs from "./Tabs_Content/Files_Archiving_tabs"
+import AssignedTask_Archiving_tabs from "./Tabs_Content/AssignedTask_Archiving_tabs"
+import MissingTask_Archiving_tabs from "./Tabs_Content/MissingTask_Archiving_tabs"
+import ArchivedTask_Archiving_tabs from "./Tabs_Content/ArchivedTask_Archiving_tabs"
 
-function SecretaryArchivingTab({ children }) {
+function SecretaryArchivingTab({ children, datas }) {
   return (
     <Tabs defaultValue="files" className="w-full">
       <TabsList>
@@ -27,10 +31,10 @@ function SecretaryArchivingTab({ children }) {
         <TabsTrigger value="missingtask">Missing Task</TabsTrigger>
         <TabsTrigger value="archivedtask">Archived Task</TabsTrigger>
       </TabsList>
-      <TabsContent value="files">{children}</TabsContent>
-      <TabsContent value="assignedtask">{children}</TabsContent>
-      <TabsContent value="missingtask">{children}</TabsContent>
-      <TabsContent value="archivedtask">{children}</TabsContent>
+      <TabsContent value="files"> <Files_Archiving_tabs dataWithFormattedDate={datas} /> </TabsContent>
+      <TabsContent value="assignedtask"> <AssignedTask_Archiving_tabs /> </TabsContent>
+      <TabsContent value="missingtask"> <MissingTask_Archiving_tabs /> </TabsContent>
+      <TabsContent value="archivedtask"><ArchivedTask_Archiving_tabs /></TabsContent>
     </Tabs>
   )
 }
---
+++ src/app/components/New_Components/Secretary/Archiving/Tabs_Content/ArchivedTask_Archiving_tabs.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+export default function ArchivedTask_Archiving_tabs() {
+    return (
+        <>
+            <div className="flex flex-col mt-5 px-10">
+                <h1 className="text-center text-[#5B0505] text-[45px] font-semibold md:shadow-zinc-400 mb-5">
+                    Archived Tasks
+                </h1>
+                <div className="flex flex-col items-center w-full">
+                    <div className="flex flex-col bg-white w-[60%] p-4 rounded-md">
+                        <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
+                            No Due Date:
+                        </label>
+                        <div className="w-full">
+                            <select
+                                className="relative flex text-xl font-bold w-full cursor-pointer bg-white h-[40px] shadow-lg rounded-sm px-2 py-1"
+                                name="taskType"
+                                id="taskType"
+                            >
+                                <option value="select"></option>
+                                <option value="nodue1">No Due Date 1</option>
+                                <option value="nodue2">No Due Date 2</option>
+                                <option value="nodue3">No Due Date 3</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col bg-white w-[60%] p-4 rounded-md">
+                        <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
+                            This week:
+                        </label>
+                        <div className="w-full">
+                            <select
+                                className="relative flex text-xl font-bold w-full cursor-pointer bg-white h-[40px] shadow-lg rounded-sm px-2 py-1"
+                                name="taskType"
+                                id="taskType"
+                            >
+                                <option value="select"></option>
+                                <option value="thisweek1">This week 1</option>
+                                <option value="thisweek2">This week 2</option>
+                                <option value="thisweek3">This week 3</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col bg-white w-[60%] p-4 rounded-md">
+                        <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
+                            Next week:
+                        </label>
+                        <div className="w-full">
+                            <select
+                                className="relative flex text-xl font-bold w-full cursor-pointer bg-white h-[40px] shadow-lg rounded-sm px-2 py-1"
+                                name="taskType"
+                                id="taskType"
+                            >
+                                <option value="select"></option>
+                                <option value="nextweek1">Next week 1</option>
+                                <option value="nextweek2">Next week 2</option>
+                                <option value="nextweek3">Next week 3</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col bg-white w-[60%] p-4 rounded-md">
+                        <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
+                            Later:
+                        </label>
+                        <div className="w-full">
+                            <select
+                                className="relative flex text-xl font-bold w-full cursor-pointer bg-white h-[40px] shadow-lg rounded-sm px-2 py-1"
+                                name="taskType"
+                                id="taskType"
+                            >
+                                <option value="select"></option>
+                                <option value="later1">Later 1</option>
+                                <option value="later2">Later 2</option>
+                                <option value="later3">Later 3</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
