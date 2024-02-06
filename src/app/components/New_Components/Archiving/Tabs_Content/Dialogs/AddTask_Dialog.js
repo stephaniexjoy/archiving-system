6d3a6f607226419aea6ee32db5c05370f71057de
xcutiepatootie <mackycalabia@gmail.com>
2024-02-06 19:50:14 +0800

    refactor: adjusted size

--- src/app/components/New_Components/Archiving/Tabs_Content/Dialogs/AddTask_Dialog.js
+++ src/app/components/New_Components/Archiving/Tabs_Content/Dialogs/AddTask_Dialog.js
@@ -33,13 +33,13 @@ function AddTask_Dialog({ sessionUser }) {
             <DialogTrigger asChild>
                 <b className='bg-transparent hover-transparent text-3xl font-bold'>Add Task</b>
             </DialogTrigger>
-            <DialogContent className="p-16 md:max-w-[1200px] h-auto">
+            <DialogContent className="p-10 md:max-w-[1200px] h-auto">
                 <div className="flex flex-col w-full">
                     <div className="flex flex-col text-3xl">
                         <Label className="text-left font-semibold">Title</Label>
                         <Input className="my-3 h-16 text-lg" type="text" placeholder="" />
                         <Label className="text-left font-semibold">Instructions</Label>
-                        <Input className="my-3 h-48 text-lg" type="text" placeholder="" />
+                        <textarea className="my-3 h-48 text-lg border rounded-lg resize-none" type="text" placeholder="" />
                         <div className="flex flex-row my-4">
                             <input
                                 type="checkbox"
