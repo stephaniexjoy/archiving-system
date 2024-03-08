c8fe02a7cc25fd2bbcf5f35d68a12d1b842c33be
beejayreyes <beejay.reyes0112@gmail.com>
2024-03-08 18:30:22 +0800

    refactor: responsive

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,20 +2,19 @@ import Sidebar from "@/app/components/Sidebar";
 import Footer from "../components/Footer";
 
 export default function DashboardLayout({
-  children // will be a page or nested layout
+  children, // will be a page or nested layout
 }) {
   return (
     <>
-
-    <div className="flex">
-      <div className="flex flex-col min-h-screen">
-        {/* Include shared UI here e.g. a header or sidebar */}
-        <div className="flex ">
-          <Sidebar className="flex" />
-          <main className=" ">{children}</main>
+      <div className="flex">
+        <div className="flex flex-col min-h-screen">
+          {/* Include shared UI here e.g. a header or sidebar */}
+          <div className="flex ">
+            <Sidebar className="flex" />
+            <main className=" ">{children}</main>
+          </div>
+          <Footer />
         </div>
-        <Footer />
-      </div>
       </div>
     </>
   );
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -9,21 +9,16 @@ import { Footer } from "flowbite-react";
 export default async function dashboard() {
   const session = await getServerSession(AuthOptions);
 
-
   if (session.user.position === "Superadmin") {
-    redirect('/superadmin/dashboard/');
+    redirect("/superadmin/dashboard/");
   }
   if (session.user.position === "Secretary") {
-  
-    redirect('/secretary/dashboard/');
+    redirect("/secretary/dashboard/");
   }
 
   const fileCount = await db.file.count();
-  
-
 
   const activities = await db.activity.findMany();
-
 
   const activitiesFormatted = activities.map((activity) => ({
     ...activity,
@@ -31,7 +26,7 @@ export default async function dashboard() {
   }));
 
   return (
-    <div className='flex flex-col w-auto h-screen'>
+    <div className="flex flex-col w-auto h-screen">
       <div className="bg-[#AD5606] w-auto h-[12%] flex items-center justify-center">
         <img
           className="w-[30%] h-[100%] object-cover flex items-center justify-center"
@@ -42,7 +37,10 @@ export default async function dashboard() {
 
       <div className="flex mt-[2%] px-10">
         <h1 className="text-black text-[20px]">
-          WELCOME, <br /> <span className='text-[#5B0505] text-[30px] font-semibold -mt-4'>FACULTY</span>
+          WELCOME, <br />{" "}
+          <span className="text-[#5B0505] text-[30px] font-semibold -mt-4">
+            FACULTY
+          </span>
         </h1>
         <img
           className="h-[50%] w-[90%] mt-[1%] ml-[3%]"
@@ -50,49 +48,43 @@ export default async function dashboard() {
           src="../photos/Filler.png"
         />
         <img
-          className="h-[70%] w-[90px] mt-[.8%] ml-[2%]"
+          className="h-[70%] w-[90px] mt-[.8%] ml-[2%] "
           alt="Notification Logo"
           src="../photos/Notificationlogo.png"
         />
       </div>
 
-      <div className="flex flex-row mt-[.5%] px-10 gap-x-12">
-        <div className='text-center bg-[#B4ABAB] text-[#5B0505] w-[440px] h-[320px] py-0 rounded-lg'>
-          <p className='mt-8 text-4xl font-semibold'>
-            ARCHIVED MATERIALS
-          </p>
-          <p className='mt-12 text-9xl text-[#5B0505] font-semibold'>
+      <div className="flex flex-col items-center justify-center content-center md:flex-row mt-[.5%] px-10 gap-x-12 ">
+        <div className="text-center bg-[#B4ABAB] text-[#5B0505] w-[440px] h-[320px] py-0 rounded-lg sm:w-[500px] sm:h-[280px] md:w-[50%] md:h-[60%] lg:w-[110%] lg:h-[60%]">
+          <p className="mt-8 text-4xl font-semibold">ARCHIVED MATERIALS</p>
+          <p className="mt-12 text-9xl text-[#5B0505] font-semibold">
             {fileCount}
           </p>
         </div>
         <div>
           <img
-            className='ml-16 w-[325px] h-auto object-cover'
+            className="ml-16 w-[325px] h-auto object-cover hidden sm:hidden md:h-[60%] lg:w-[110%] lg:h-[60%] "
             src="../photos/ccsLogo.png"
             alt="CCS Logo"
           />
         </div>
-        <div className='ml-10 text-center bg-[#B4ABAB] text-[#5B0505] w-[500px] h-[540px]'>
-          <p className=' mt-5 text-1xl font-bold'>
-            CALENDAR
-          </p>
+        <div className=" text-center bg-[#B4ABAB] text-[#5B0505] w-[500px] sm:w-[500px] sm:h-[540px] mt-4 md:-0 md:w-full">
+          <p className=" mt-1 text-1xl font-bold">CALENDAR</p>
         </div>
       </div>
 
       <div className="flex flex-col -mt-[11%] w-[50%] px-10">
-        <div className='mt-[1%] text-center bg-[#5B0505] text-white h-[90px] rounded-lg'>
-          <button className='mt-2 text-4xl font-semibold'>
+        <div className="mt-[1%] text-center bg-[#5B0505] text-white h-[90px] rounded-lg">
+          <button className="mt-2 text-4xl font-semibold">
             <EditPrivacy_Dialog sessionUser={session.user} />
           </button>
         </div>
-        <div className='mt-[2%] text-center bg-[#5B0505] text-white h-[90px] rounded-lg'>
-          <button className='mt-4 text-4xl font-semibold'>
+        <div className="mt-[2%] text-center bg-[#5B0505] text-white h-[90px] rounded-lg">
+          <button className="mt-4 text-4xl font-semibold">
             <UploadMaterial_Dialog sessionUser={session.user} />
           </button>
         </div>
       </div>
-
-
     </div>
   );
 }
