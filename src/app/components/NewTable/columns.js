9317fd4ace9270d262a8ed205169b5aaa49018dd
Asdf123ah <0320-0770@lspu.edu.ph>
2024-03-14 15:52:21 +0800

    edit columns

--- src/app/components/NewTable/columns.js
+++ src/app/components/NewTable/columns.js
@@ -24,12 +24,11 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import AddCategory_Dialog from "@/app/components/New_Components/AddCategory_Dialog/AddCategory_Dialog";
-
-
+import Material_ComboBox from "../New_Components/Archiving/Tabs_Content/ComboBox/Material_ComboBox";
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
 
-export const columns = [  
+export const columns = [
   {
     accessorKey: "filename",
     header: ({ column }) => {
@@ -91,91 +90,32 @@ export const columns = [
               <DialogTrigger className="bg-[#8F8F8F] hover:bg-[#6A6A6A] focus:outline-none h-10 w-48 font-bold">
                 Edit
               </DialogTrigger>
-              <DialogContent className="bg-white max-w-[700px] max-h-[600px] h-auto py-6 px-6 mx-auto overflow-y-auto">
+              <DialogContent className="bg-white max-w-[700px] max-h-[800px] h-auto py-6 px-6 mx-auto overflow-y-auto">
                 <DialogHeader>
                   <DialogTitle className="text-2xl">Edit</DialogTitle>
                   <DialogDescription>
                     {/* You can upload files here. */}
                     <AddCategory_Dialog category={"Material"} />
                     <AddCategory_Dialog category={"Course"} />
+                    <br />
+                    <div className="flex flex-row font-semibold text-[#5B0505] w-full gap-x-0 py-5 px-10 ml-0">
+                      <div className="ml-8 inline-block text-[20px] md:text-[23px]">
+                        MATERIAL
+                      </div>
+                      <div className="ml-20 inline-block text-[20px] md:text-[23px]">
+                        PROGRAM
+                      </div>
+                      <div className="ml-20 inline-block text-[20px] md:text-[23px]">
+                        INSTRUCTOR
+                      </div>
+                    </div>
+                    <div className="flex flex-row gap-x-28 w-full px-10">
+                      <Material_ComboBox materialFrameworks={materials} />
+                    </div>
                   </DialogDescription>
                 </DialogHeader>
-                <div className="flex flex-col w-full">
-
-                </div>
-                <div className="flex flex-col bottom-0 items-center justify-center w-full h-auto">
-                  {/* <label
-                    htmlFor="file-upload"
-                    className="w-[40%] h-10 border bg-[#AD5606] hover:bg-gray-700 text-xl text-white font-semibold py-1 px-4 my-2 cursor-pointer inline-flex items-center justify-center rounded-lg"
-                  >
-                    Browse
-                  </label>{" "} */}
-                  {/* <AlertDialog>
-                    <AlertDialogTrigger asChild>
-                      <button className="w-[40%] h-10 border bg-[#AD5606] hover:bg-gray-700 text-xl text-white font-semibold py-1 px-4 my-2 cursor-pointer items-center justify-center rounded-lg">
-                        Upload
-                      </button>
-                    </AlertDialogTrigger>
-                    <AlertDialogContent>
-                      <AlertDialogHeader>
-                        <AlertDialogTitle>
-                          Do you want to Proceed?
-                        </AlertDialogTitle>
-                        <AlertDialogDescription>
-                          This action will upload your file temporarily. Mark it
-                          as done to confirm the upload.
-                        </AlertDialogDescription>
-                      </AlertDialogHeader>
-                      <AlertDialogFooter className="items-center">
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <AlertDialogAction
-                          onClick={async () => {
-                            console.log(uploadedFiles);
-
-                            try {
-                              const uploadPromises = uploadedFiles.map(
-                                async (upFile) => {
-                                  try {
-                                    console.log("Upfile", upFile);
-                                    const res =
-                                      await edgestore.publicFiles1.upload({
-                                        file: upFile,
-                                        options: {
-                                          manualFileName: upFile.name,
-                                          temporary: true,
-                                        },
-                                      });
-                                    console.log(res);
-                                    return res;
-                                  } catch (error) {
-                                    console.error(error);
-                                    throw error; // rethrowing error to handle at the end
-                                  }
-                                }
-                              );
-
-                              const results = await Promise.all(uploadPromises);
-                              if (results) {
-                                toast;
-                              }
-
-                              getUrls(results, setUrls);
-                              console.log(
-                                "All files uploaded successfully:",
-                                results
-                              );
-                            } catch (error) {
-                              console.error("Error uploading files:", error);
-                            }
-                            console.log(urls);
-                          }}
-                        >
-                          Continue
-                        </AlertDialogAction>
-                      </AlertDialogFooter>
-                    </AlertDialogContent>
-                  </AlertDialog>{" "} */}
-                </div>
+                <div className="flex flex-col w-full"></div>
+                <div className="flex flex-col bottom-0 items-center justify-center w-full h-auto"></div>
               </DialogContent>
             </Dialog>
           </Button>
