f0801cff0d4e432b8a345f21037763aa7d701102
xcutiepatootie <mackycalabia@gmail.com>
2024-03-14 22:06:17 +0800

    refactor: mobile responsive login

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import Login from './components/Login'
 
 export default function Home() {
   return (
-    <main className="bg-[#5B0505] flex min-h-screen flex-col items-center justify-between p-0">
+    <main className="">
 
       <section id='home'>
         <Login />
