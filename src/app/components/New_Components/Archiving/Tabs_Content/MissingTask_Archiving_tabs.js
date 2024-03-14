183205a5f142f70f3c98c97aae3ad5decad34eb6
Asdf123ah <0320-0770@lspu.edu.ph>
2024-03-14 15:09:42 +0800

    edit missing tasks

--- src/app/components/New_Components/Archiving/Tabs_Content/MissingTask_Archiving_tabs.js
+++ src/app/components/New_Components/Archiving/Tabs_Content/MissingTask_Archiving_tabs.js
@@ -35,31 +35,31 @@ export default function MissingTask_Archiving_tabs() {
               {options === "nodue1" && (
                 <div class="flex flex-row mt-4">
                   <div class="w-full p-0">
-                    <Card>
+                    <Card className="w-full h-auto">
                       <CardContent>
                         <div class="flex flex-row text-xl text-black p-4 mt-4">
                           <div className="w-full flex flex-col">
-                            <h1 class="text-2xl font-semibold mb-12">
+                            <h1 class="text-4xl font-semibold mb-52">
                               No Due Date 1 Title
                             </h1>
-                            <p class="mb-16 text-lg">
+                            <p class="mb-52 text-2xl">
                               No Due Date 1 Instructions.
                             </p>
                             <div>
-                              <h1 class="text-sm mb-0">Post Today</h1>
+                              <h1 class="text-lg ">Post Today</h1>
                             </div>
                           </div>
-                          <div class="w-[60%]">
+                          <div class="w-[80%] h-auto">
                             <div class="flex flex-col bg-white p-4 gap-4 drop-shadow-2xl  rounded-xl">
-                              <div className="flex flex-row gap-x-48">
-                                <h1 className="text-md">Your work</h1>
-                                <h1 className="text-sm text-red-600">
+                              <div className="flex flex-row gap-x-56">
+                                <h1 className="text-xl">Your work</h1>
+                                <h1 className="text-md text-red-600">
                                   Missing
                                 </h1>
                               </div>
                               <label
                                 for="file-upload"
-                                class="w-full h-10 border bg-white hover:bg-gray-100 text-[#AD5606] font-bold py-1 px-4 rounded my-2 cursor-pointer inline-flex items-center justify-center"
+                                class="w-full h-14 border bg-white hover:bg-gray-100 text-[#AD5606] font-bold py-1 px-4 rounded my-2 cursor-pointer inline-flex items-center justify-center mt-16"
                               >
                                 Add work
                                 <input
@@ -68,7 +68,7 @@ export default function MissingTask_Archiving_tabs() {
                                   class="hidden"
                                 />
                               </label>{" "}
-                              <button class="w-full h-10 border bg-[#AD5606] hover:bg-[#AD5606]-700 text-white font-bold py-1 px-4 rounded">
+                              <button class="w-full h-14 border bg-[#AD5606] hover:bg-[#AD5606]-700 text-white font-bold py-1 px-4 rounded">
                                 Mark as done
                               </button>
                             </div>
