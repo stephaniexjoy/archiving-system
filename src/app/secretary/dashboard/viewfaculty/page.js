480bd5a536b9735f8e5559adbe556315d86ba4e0
xcutiepatootie <mackycalabia@gmail.com>
2024-03-15 00:00:44 +0800

    style: removed searchbar

--- src/app/secretary/dashboard/viewfaculty/page.js
+++ src/app/secretary/dashboard/viewfaculty/page.js
@@ -1,11 +1,10 @@
-
-import React from 'react'
+import React from "react";
 import { FaSearch } from "react-icons/fa";
 import CreateModal from "@/app/components/Modal/CreateModal";
 import DeleteModal from "@/app/components/Modal/DeleteModal";
 import { revalidatePath } from "next/cache";
 import Link from "next/link";
-import { Table, TableCell, TableRow } from '@/components/ui/table';
+import { Table, TableCell, TableRow } from "@/components/ui/table";
 
 async function page() {
   async function getEmptyData() {
@@ -13,15 +12,15 @@ async function page() {
   }
 
   async function getUsers() {
-    "use server"
-    const res = await fetch('http://localhost:3000/api/users/fetch-users',)
+    "use server";
+    const res = await fetch("http://localhost:3000/api/users/fetch-users");
     if (!res.ok) {
-      return getEmptyData()
+      return getEmptyData();
     }
-    revalidatePath('/secretary/dashboard/viewfaculty/')
-    return res.json()
+    revalidatePath("/secretary/dashboard/viewfaculty/");
+    return res.json();
   }
-  const users = await getUsers()
+  const users = await getUsers();
   return (
     <div className="flex flex-col w-screen h-screen">
       <div className="bg-[#AD5606] w-full h-[12%] flex items-center justify-center">
@@ -33,24 +32,8 @@ async function page() {
       </div>
 
       <div className="bg-[#5B0505] flex flex-row mb-5 md:h-20 px-10">
-        <h1 className="text-[#DABB9C] mt-2 w-auto text-[45px] font-semibold md:shadow-zinc-400 px-24">
-          FIND
-        </h1>
-        <div className="flex items-center">
-          <input
-            type="search"
-            name="find"
-            placeholder=""
-            className="mt-1 ml-2 mr-3 text-md text-[#242323] bg-[#D9D9D9] w-[1300px] h-[60px] md:text-shadow-inner"
-          />
-          <div className="mt-1 ml-4 text-white bg-[#6A6A6A] p-2 h-[60px] w-auto">
-            <FaSearch size="50" />
-          </div>
-        </div>
         <div className="mt-3 bg-[#6A6A6A] p-2 h-[60px] w-[100px] ml-[1%]">
-          <button className="mt-2 ml-1 text-white text-[20px]">
-            CREATE
-          </button>
+          <button className="mt-2 ml-1 text-white text-[20px]">CREATE</button>
         </div>
         <div className="mt-3 bg-[#6A6A6A] p-2 h-[60px] w-[70px] ml-[1%]">
           <img
@@ -68,7 +51,10 @@ async function page() {
         <div className="container flex justify-center mx-auto drop-shadow-2xl">
           <div className="flex flex-col">
             <div className="w-full">
-              <Table className="w-[1300px] divide-y divide-gray-300" style={{ borderCollapse: 'separate', borderSpacing: '0 30px' }}>
+              <Table
+                className="w-[1300px] divide-y divide-gray-300"
+                style={{ borderCollapse: "separate", borderSpacing: "0 30px" }}
+              >
                 <Table className="bg-[#837979] divide-y divide-gray-300">
                   {users.map((user) => (
                     <TableRow key={user.id} className="whitespace-nowrap">
@@ -80,7 +66,12 @@ async function page() {
                       {/* <TableCell></TableCell>
                       <TableCell></TableCell> */}
                       <TableCell className="px-1 py-4">
-                        <Link href={`/secretary/dashboard/viewfaculty/view-user/${user.id}/`} className="px-4 py-1 text-base font-semibold text-white bg-[#675454] rounded-md">VIEW</Link>
+                        <Link
+                          href={`/secretary/dashboard/viewfaculty/view-user/${user.id}/`}
+                          className="px-4 py-1 text-base font-semibold text-white bg-[#675454] rounded-md"
+                        >
+                          VIEW
+                        </Link>
                       </TableCell>
                     </TableRow>
                   ))}
@@ -94,4 +85,4 @@ async function page() {
   );
 }
 
-export default page
+export default page;
