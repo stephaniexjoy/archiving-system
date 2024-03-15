c51e65051d0b5b80a765729f4309be4ea8c70439
beejayreyes <beejay.reyes0112@gmail.com>
2024-03-15 18:06:16 +0800

    refactor: footer boss all goods na <3333

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#AD5606] px-4 md:px-10 h-[30%]">
-      <div className="flex flex-row md:flex-row">
+    <footer className="bg-[#AD5606] px-4 md:px-10 h-[105%]">
+      <div className="flex flex-col md:flex-row">
         <div className="flex flex-col md:flex-col md:w-1/2">
           <div className="flex flex-col">
             <div className="flex items-start justify-start mb-4 md:mb-0 mr-0 md:mr-4">
@@ -14,20 +14,20 @@ const Footer = () => {
             </div>
           </div>
           <div className="flex flex-col md:flex-row w-full md:w-1/2">
-            <div className="w-full md:w-1/2 flex flex-col items-start justify-start -mt-[14%]">
+            <div className="w-full md:w-1/2 flex flex-col items-start justify-start -mt-[5%]">
               <p className="text-[#5B0505] text-lg font-semibold mr-2">CONTACT US</p>
               <div className="flex flex-col">
                 <div className="flex items-start justify-start mb-2">
                   <img src="/photos/callLogo.png" alt="Call Logo" className="w-7 h-auto" />
-                  <p className="ml-1 text-white font-semibold">(049) 554-3033</p>
+                  <p className="ml-1 text-white text-[12px] font-semibold">(049) 554-3033</p>
                 </div>
                 <div className="flex items-start justify-start mb-2">
                   <img src="/photos/emailLogo.png" alt="Email Logo" className="w-7 h-auto" />
-                  <p className="ml-1 text-white font-semibold">ccs_spc@lspu.edu.ph</p>
+                  <p className="ml-1 text-white text-[12px] font-semibold">ccs_spc@lspu.edu.ph</p>
                 </div>
                 <div className="flex items-start justify-start">
                   <img src="/photos/locationLogo.png" alt="Location Logo" className="w-7 h-auto" />
-                  <p className="ml-1 text-white font-semibold">Brgy. Del Remedio, San Pablo City, Philippines</p>
+                  <p className="ml-1 text-white text-[12px] font-semibold">Brgy. Del Remedio, San Pablo City, Philippines</p>
                 </div>
               </div>
             </div>
@@ -35,7 +35,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-col w-full md:w-1/2">
           <div className="text-[#5B0505] text-lg font-semibold mb-2 mt-2">ABOUT</div>
-          <p className="text-white text-sm md:text-base mb-4">
+          <p className="text-white text-[12px] md:text-base mb-4 text-justify">
             The E-Archiving System endeavors to address the challenges posed by traditional document storage and retrieval methods, aiming to streamline processes, improve efficiency, and provide a comprehensive solution for the dynamic environment of the CCS. Recognizing the significance of accessible and organized data, our team has rallied with a shared commitment to bring forth a solution that not only meets the current needs of the faculty but also anticipates future demands.
           </p>
         </div>
