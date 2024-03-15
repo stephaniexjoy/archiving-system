87abf075242a8d6ffefb078195b2efe705ec63a2
Asdf123ah <0320-0770@lspu.edu.ph>
2024-03-15 19:37:45 +0800

    edit forgot password

--- src/app/components/New_Components/ForgotPassword_Dialog.js
+++ src/app/components/New_Components/ForgotPassword_Dialog.js
@@ -19,14 +19,14 @@ export default function ForgotPassword_Dialog() {
     return (
         <Dialog>
             <DialogTrigger asChild>
-                <span className="
+                <b className="
                 border-none ml-40 mt-6 text-[9px] w-auto [text-decoration:underline] inline-block text-[#FF7E7E] cursor-pointer
                 sm:border-none sm:ml-72 sm:mt-6 sm:text-sm sm:w-auto sm:[text-decoration:underline] sm:inline-block sm:text-[#FF7E7E] sm:cursor-pointer
                 md:border-none md:ml-[75%] md:mt-6 md:text-md md:w-auto md:[text-decoration:underline] md:inline-block md:text-[#FF7E7E] md:cursor-pointer
                 lg:border-none lg:ml-[76%] lg:mt-6 lg:text-lg lg:w-auto lg:[text-decoration:underline] lg:inline-block lg:text-[#FF7E7E] lg:cursor-pointer
                 xl:border-none xl:ml-[68%] xl:mt-6 xl:text-xl xl:w-auto xl:[text-decoration:underline] xl:inline-block xl:text-[#FF7E7E] xl:cursor-pointer
                 2xl:border-none 2xl:ml-[72%] 2xl:mt-10 2xl:text-2xl 2xl:w-auto 2xl:[text-decoration:underline] 2xl:inline-block 2xl:text-[#FF7E7E] 2xl:cursor-pointer
-                ">Forgot Password?</span>
+                ">Forgot Password?</b>
             </DialogTrigger>
             <DialogContent className="w-full p-8">
                 <DialogHeader className="space-y-6">
