bc6a1c264e9cde2c16d601481fcff27081b5fca3
beejayreyes <beejay.reyes0112@gmail.com>
2024-03-15 17:40:28 +0800

    refactor: mobile responsive lang, wala pang sm, md, lg, xl, 2xl. Labyu

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,39 +2,39 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-[#AD5606] px-4 md:px-10">
+    <footer className="bg-[#AD5606] px-4 md:px-10 h-[30%]">
       <div className="flex flex-row md:flex-row">
-        <div className="flex flex-col md:flex-col w-full md:w-1/2">
-          <div className="flex flex-row md:flex-row">
+        <div className="flex flex-col md:flex-col md:w-1/2">
+          <div className="flex flex-col">
             <div className="flex items-start justify-start mb-4 md:mb-0 mr-0 md:mr-4">
-              <img src="/photos/lspuLogo.png" alt="LSPU Logo" className="w-12 h-auto" />
+              <img src="/photos/lspuLogo.png" alt="LSPU Logo" className="w-0 h-0" />
             </div>
             <div className="flex items-start justify-start mb-4 md:mb-0">
-              <img src="/photos/ccsLogo.png" alt="CCS Logo" className="w-12 h-auto" />
+              <img src="/photos/ccsLogo.png" alt="CCS Logo" className="w-0 h-0" />
             </div>
           </div>
           <div className="flex flex-col md:flex-row w-full md:w-1/2">
-            <div className="w-full md:w-1/2 flex items-start justify-start">
+            <div className="w-full md:w-1/2 flex flex-col items-start justify-start -mt-[14%]">
               <p className="text-[#5B0505] text-lg font-semibold mr-2">CONTACT US</p>
               <div className="flex flex-col">
                 <div className="flex items-start justify-start mb-2">
                   <img src="/photos/callLogo.png" alt="Call Logo" className="w-7 h-auto" />
-                  <p className="ml-3 text-white font-semibold">(049) 554-3033</p>
+                  <p className="ml-1 text-white font-semibold">(049) 554-3033</p>
                 </div>
                 <div className="flex items-start justify-start mb-2">
                   <img src="/photos/emailLogo.png" alt="Email Logo" className="w-7 h-auto" />
-                  <p className="ml-3 text-white font-semibold">ccs_spc@lspu.edu.ph</p>
+                  <p className="ml-1 text-white font-semibold">ccs_spc@lspu.edu.ph</p>
                 </div>
                 <div className="flex items-start justify-start">
                   <img src="/photos/locationLogo.png" alt="Location Logo" className="w-7 h-auto" />
-                  <p className="ml-3 text-white font-semibold">Brgy. Del Remedio, San Pablo City, Philippines</p>
+                  <p className="ml-1 text-white font-semibold">Brgy. Del Remedio, San Pablo City, Philippines</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="flex flex-col w-full md:w-1/2">
-          <div className="text-[#5B0505] text-3xl font-bold mb-2">ABOUT</div>
+          <div className="text-[#5B0505] text-lg font-semibold mb-2 mt-2">ABOUT</div>
           <p className="text-white text-sm md:text-base mb-4">
             The E-Archiving System endeavors to address the challenges posed by traditional document storage and retrieval methods, aiming to streamline processes, improve efficiency, and provide a comprehensive solution for the dynamic environment of the CCS. Recognizing the significance of accessible and organized data, our team has rallied with a shared commitment to bring forth a solution that not only meets the current needs of the faculty but also anticipates future demands.
           </p>
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -26,38 +26,38 @@ export default async function dashboard() {
   }));
 
   return (
-    <div className="flex flex-col w-auto h-full">
-      <div className="bg-[#AD5606] w-auto h-[80%] flex items-center justify-center">
+    <div className="flex flex-col w-[250%] h-[100%]">
+      <div className="bg-[#AD5606] w-[40%] h-[80%] flex items-center justify-center">
         <img
-          className="sm:w-[27%] sm:h-[100%] md:w-[35%] md:h-[100%] lg:w-[27%] lg:h-[100%] object-cover flex items-center justify-center"
+          className="w-[50%] sm:w-[27%] sm:h-[100%] md:w-[35%] md:h-[100%] lg:w-[27%] lg:h-[100%] object-cover items-center justify-center"
           alt="E-Archiving System"
           src="/photos/E-Archiving System.png"
         />
       </div>
 
-      <div className="flex mt-[2%] px-10">
-        <h1 className="text-black text-[20px] sm:text-sm md:text-md lg:text-lg">
+      <div className="flex mt-[2%] px-10 w-[23%] -ml-[4%]">
+        <h1 className="text-black text-[15px]">
           WELCOME, <br />{" "}
-          <span className="text-[#5B0505] text-[30px] font-semibold -mt-4">
+          <span className="text-[#5B0505] text-[20px] font-semibold -mt-4">
             FACULTY
           </span>
         </h1>
         <img
-          className="h-[50%] w-[90%] mt-[1%] ml-[3%] sm:w-[80%] sm:h-[40%] md:w-[80%] md:h-[40%] lg:w-[70%] lg:h-[50%]"
+          className="w-[300%] h-[30%] mt-[10%] ml-[30%]"
           alt="Filler"
           src="../photos/Filler.png"
         />
         <img
-          className="h-[70%] w-[90px] mt-[.8%] ml-[2%] sm:w-[50%] sm:h-[50%] md:w-[60%] md:h-[50%] lg:w-[10%] lg:h-[50%]"
+          className="h-[70%] w-[90px] mt-[.8%] ml-[2%]"
           alt="Notification Logo"
           src="../photos/Notificationlogo.png"
         />
       </div>
 
-      <div className="flex flex-row items-center justify-center content-center sm:grid-row-2 gap-4 sm:mt-3 lg:mt-20 lg:ml-3">
-       <div className="text-center bg-[#B4ABAB] text-[#5B0505] w-[440px] h-[320px] py-0 rounded-lg sm:w-[300px] sm:h-[280px] md:w-[50%] md:h-[60%] lg:w-[50%] lg:h-[150%]">
-          <p className="mt-8 text-4xl font-semibold">ARCHIVED MATERIALS</p>
-          <p className="mt-20 text-8xl text-[#5B0505] font-semibold">
+      <div className="flex flex-col items-center justify-center content-center -ml-[64%]">
+       <div className="text-center bg-[#B4ABAB] text-[#5B0505]  py-0 rounded-lg w-[20%]  h-96 ">
+          <p className="mt-7 text-2xl font-bold">ARCHIVED MATERIALS</p>
+          <p className="mt-5 text-4xl text-[#5B0505] font-semibold">
             {fileCount}
           </p>
         </div>
@@ -70,12 +70,12 @@ export default async function dashboard() {
           />
         </div>
 
-        <div className=" text-center bg-[#B4ABAB] text-[#5B0505] ml-4 sm:w-[300px] sm:h-[280px] mt-4 md:-0 md:w-full lg:w-[50%] lg:h-[150%]">
-          <p className=" mt-1 text-4xl font-bold">CALENDAR</p>
+        <div className=" text-center bg-[#B4ABAB] text-[#5B0505] mt-4 py-0 rounded-lg w-[20%] h-[100%]">
+          <p className=" mt-1 text-3xl font-semibold">CALENDAR</p>
         </div>
       </div>
 
-      <div class="flex flex-col mt-[10%] w-full sm:w-[50%] gap-4 m-4">
+      <div class="flex flex-col mt-[5%] w-[35%] ml-2">
   <div class="flex flex-col sm:flex-row gap-4">
     <div class="text-center bg-[#5B0505] text-white w-full sm:w-[300px] h-[90px] rounded-lg">
       <button class="mt-2 text-4xl font-semibold sm:text-3xl">
