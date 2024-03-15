f1e57dac24427fa8005dfafcb8c9da71d15875f4
beejayreyes <beejay.reyes0112@gmail.com>
2024-03-15 15:24:20 +0800

    refactor: footer

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -2,20 +2,21 @@ import Sidebar from "@/app/components/Sidebar";
 import Footer from "../components/Footer";
 
 export default function DashboardLayout({
-  children // will be a page or nested layout
+  children, // will be a page or nested layout
 }) {
   return (
     <>
-
-    <div className="flex">
-      <div className="flex flex-col min-h-screen">
-        {/* Include shared UI here e.g. a header or sidebar */}
-        <div className="flex ">
-          <Sidebar className="flex" />
-          <main className=" ">{children}</main>
+      <div className="flex">
+        <div className="flex flex-col min-h-screen">
+          {/* Include shared UI here e.g. a header or sidebar */}
+          <div className="flex ">
+            <Sidebar className="flex" />
+            <main className=" ">{children}</main>
+          </div>
+          <div className="">
+            <Footer />
+          </div>
         </div>
-        <Footer />
-      </div>
       </div>
     </>
   );
