eb30eddc4a86a334f7d03d113eea1e781856cddc
xcutiepatootie <mackycalabia@gmail.com>
2024-03-18 14:53:04 +0800

    feat: forgot password function, fix images

--- src/app/page.js
+++ src/app/page.js
@@ -6,15 +6,21 @@ export default async function Home() {
   /*  const send = async () => {
     "use server";
     await sendMail({
-      to: "mackycalabia@gmail.com",
+      to: "matienzonoreenma.patrice@gmail.com",
       name: "aj",
       subject: "Forgot Pass",
-      body:  `<h1>yabag</h1>`  compileEmailTemplate("Test", "youtube"),
+      body: `<h1>yabag</h1>`  compileEmailTemplate(
+        "Test",
+        "fb.com"
+      ),
     });
   }; */
   return (
     <main className="">
       <section id="home">
+        {/* <form>
+          <Button formAction={send}>test</Button>
+        </form> */}
         <Login />
       </section>
     </main>
