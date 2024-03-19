03fb8f75f97d3877a6989df43e3ee68648d2a0ae
beejayreyes <beejay.reyes0112@gmail.com>
2024-03-19 22:30:30 +0800

    refactor: skeleton of both faculty and secretary dashboard

--- src/app/components/Skeletons/Secretary/Dashboard/Dashboard_Skeleton.js
+++ src/app/components/Skeletons/Secretary/Dashboard/Dashboard_Skeleton.js
@@ -13,12 +13,12 @@ export default function Dashboard_Skeleton() {
       <div className=" flex mt-[2%] px-10 ">
         {/* welcome */}
         <div>
-          <Skeleton className="ml-2 h-8 w-20 bg-gray-400" />
+          <Skeleton className="ml-2 h-8 w-20 bg-gray-500" />
         </div>
 
         {/* secretary */}
         <div>
-          <Skeleton className="-ml-20 mt-12 h-9 w-44 bg-gray-400" />
+          <Skeleton className="-ml-20 mt-12 h-9 w-44 bg-gray-500" />
         </div>
 
         {/* filler */}
@@ -36,29 +36,29 @@ export default function Dashboard_Skeleton() {
       <div className=" flex flex-row ml-7 mt-[2%] gap-x-11">
         {/* archived materials */}
         <div className="bg-gray-400 mt-5 w-[19%] h-[320px] rounded-lg">
-          <Skeleton className=" h-16 w-48 ml-12 mt-10 bg-red-400" />
-          <Skeleton className="mt-20 ml-28 h-24 w-20 bg-red-400" />
+          <Skeleton className="h-16 w-48 ml-12 mt-10 bg-gray-500" />
+          <Skeleton className="mt-20 ml-28 h-24 w-20 bg-gray-500" />
         </div>
 
         {/* no. of faculty */}
         <div className=" bg-gray-400 mt-5 w-[19%] h-[320px] rounded-lg">
-          <Skeleton className=" h-16 w-48 ml-10 mt-20 bg-red-400" />
-          <Skeleton className=" h-24 w-20 ml-24 mt-12 bg-red-400" />
+          <Skeleton className=" h-16 w-48 ml-10 mt-20 bg-gray-500" />
+          <Skeleton className=" h-24 w-20 ml-24 mt-12 bg-gray-500" />
         </div>
 
         {/* parent div of create account, view materials, view faculty */}
         <div className="flex flex-col items-center ml-28  mt-6">
           {/* create account */}
           <div className=" bg-gray-400 w-[500%] h-[90px] p-4 rounded-lg">
-            <Skeleton className="h-12 w-[100%] ml-1 mt-1 bg-red-400" />
+            <Skeleton className="h-12 w-[100%] ml-1 mt-1 bg-gray-500" />
           </div>
           {/* view materials */}
           <div className="mt-6 bg-gray-400 w-[500%] h-[90px] p-4 :mx-4 rounded-lg">
-            <Skeleton className="h-10 w-[100%] mt-2 bg-red-400" />
+            <Skeleton className="h-10 w-[100%] mt-2 bg-gray-500" />
           </div>
           {/* view faculty */}
           <div className="mt-6 bg-gray-400 w-[500%] h-[90px] p-4 mx-4 rounded-lg">
-            <Skeleton className="h-10 w-[100%] mt-2 bg-red-400" />
+            <Skeleton className="h-10 w-[100%] mt-2 bg-gray-500" />
           </div>
         </div>
 
@@ -69,13 +69,13 @@ export default function Dashboard_Skeleton() {
       </div>
       {/* filter users */}
       <div className="bg-gray-400 w-[35%] h-12 mt-14 ml-9 rounded-lg">
-        <Skeleton className="h-8 w-24 ml-2 mt-2 bg-red-400" />
+        <Skeleton className="h-8 w-24 ml-2 mt-2 bg-gray-500" />
       </div>
       <div className="bg-gray-400 w-[94%] h-12 mt-3 ml-9">
-        <Skeleton className="h-8 w-24 ml-2 mt-2 bg-red-400" />
-        <Skeleton className="h-8 w-24 ml-[20%] -mt-8 bg-red-400" />
-        <Skeleton className="h-8 w-24 ml-[35%] -mt-8 bg-red-400" />
-        <Skeleton className="h-8 w-24 ml-[73%] -mt-8 bg-red-400" />
+        <Skeleton className="h-8 w-24 ml-2 mt-2 bg-gray-500" />
+        <Skeleton className="h-8 w-24 ml-[20%] -mt-8 bg-gray-500" />
+        <Skeleton className="h-8 w-24 ml-[35%] -mt-8 bg-gray-500" />
+        <Skeleton className="h-8 w-24 ml-[73%] -mt-8 bg-gray-500" />
 
       </div>
     </div>
