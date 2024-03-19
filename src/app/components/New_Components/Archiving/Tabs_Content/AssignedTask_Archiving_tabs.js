fc05f01485a78870edade00802f57bf43f39643a
Asdf123ah <0320-0770@lspu.edu.ph>
2024-03-19 22:13:45 +0800

    edit assigned tasks

--- src/app/components/New_Components/Archiving/Tabs_Content/AssignedTask_Archiving_tabs.js
+++ src/app/components/New_Components/Archiving/Tabs_Content/AssignedTask_Archiving_tabs.js
@@ -96,7 +96,15 @@ export default function AssignedTask_Archiving_tabs({
 
   return (
     <>
-      <div className="flex flex-col mt-16 px-10 space">
+      <div 
+        className="
+        flex flex-col mt-16 px-4 space h-auto overflow-y-auto
+        sm:flex sm:flex-col sm:mt-16 sm:px-10 sm:space sm:h-auto sm:overflow-y-auto
+        md:flex md:flex-col md:mt-16 md:px-10 md:space md:h-auto md:overflow-y-auto
+        lg:flex lg:flex-col lg:mt-16 lg:px-10 lg:space lg:h-auto lg:overflow-y-auto
+        xl:flex xl:flex-col xl:mt-16 xl:px-10 xl:space xl:h-auto xl:overflow-y-auto
+        2xl:flex 2xl:flex-col 2xl:mt-16 2xl:px-10 2xl:space 2xl:h-auto 2xl:overflow-y-auto
+        ">
         <h1 className="text-center text-[#5B0505] text-[28px]  sm:text-[32px] md:text-[36px] lg:text-[45px] font-bold md:shadow-zinc-400 mb-5 ">
           Assigned Tasks
         </h1>
@@ -104,8 +112,12 @@ export default function AssignedTask_Archiving_tabs({
           <div className="flex items-center justify-center">
             <button
               className="
-            bg-[#AD5606] text-white text-center items-center w-36 h-10 border border-black text-lg rounded-lg mb-5
-            2xl:bg-[#AD5606] 2xl:text-white 2xl:text-center 2xl:items-center 2xl:w-[15%] 2xl:h-12 2xl:border 2xl:border-black 2xl:text-xl 2xl:rounded-lg
+            bg-[#AD5606] text-white text-center items-center w-36 h-10 border border-black text-lg rounded-lg mb-10
+            sm:bg-[#AD5606] sm:text-white sm:text-center sm:items-center sm:w-36 sm:h-10 sm:border sm:border-black sm:text-lg sm:rounded-lg sm:mb-10
+            md:bg-[#AD5606] md:text-white md:text-center md:items-center md:w-36 md:h-10 md:border md:border-black md:text-lg md:rounded-lg md:mb-10
+            lg:bg-[#AD5606] lg:text-white lg:text-center lg:items-center lg:w-36 lg:h-10 lg:border lg:border-black lg:text-lg lg:rounded-lg lg:mb-10
+            xl:bg-[#AD5606] xl:text-white xl:text-center xl:items-center xl:w-36 xl:h-10 xl:border xl:border-black xl:text-lg xl:rounded-lg xl:mb-10
+            2xl:bg-[#AD5606] 2xl:text-white 2xl:text-center 2xl:items-center 2xl:w-[15%] 2xl:h-12 2xl:border 2xl:border-black 2xl:text-xl 2xl:rounded-lg 2xl:mt-10
             "
             >
               <AddTask_Dialog />
@@ -113,15 +125,23 @@ export default function AssignedTask_Archiving_tabs({
           </div>
         )}
 
-        <div className="flex flex-col items-center w-full">
-          <div className="flex flex-col bg-white w-[80%] lg:w-[60%] md:w-[60%] sm:[60%] p-4 rounded-md ">
-            <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
+        <div className="flex flex-col items-center w-full h-full overflow-y-auto">
+          <div 
+            className="
+            flex flex-col w-[100%] p-4 rounded-md 
+            sm:[60%]
+            md:w-[60%] 
+            lg:w-[60%]
+            xl:w-[60%] 
+            2xl:w-[60%] 
+            ">
+            <label className="w-full text-[#5B0505] text-xl text-start font-semibold mr-4">
               No Due Date:
             </label>
             <div className="w-full space-y-2">
               <select
                 onChange={(e) => setOptions(e.target.value)}
-                className="relative flex text-xl font-bold w-full cursor-pointer bg-white h-[40px] shadow-lg rounded-sm px-2 py-1 border border-orange-900"
+                className="relative flex text-xl font-bold w-full cursor-pointe h-[40px] shadow-lg rounded-sm px-2 py-2 border border-orange-900"
                 name="taskType"
                 id="taskType"
               >
@@ -137,15 +157,42 @@ export default function AssignedTask_Archiving_tabs({
                 (task, index) =>
                   options === String(task.id) && (
                     <div key={`${task.id}_${index}`}>
-                      <div className="flex flex-row items-center">
+                      <div 
+                        className="
+                        flex flex-row items-center
+                        xl:flex xl:flex-col xl:items-center
+                        2xl:flex 2xl:flex-row 2xl:items-center
+                        ">
                         <div className="w-full p-0">
-                          <Card className="lg:w-full w-[330px] h-auto">
+                          <Card 
+                            className="
+                            w-full h-auto
+                            sm:w-full sm:h-auto
+                            md:w-full md:h-auto
+                            lg:w-full lg:h-auto
+                            xl:w-full xl:h-auto
+                            2xl:w-full 2xl:h-auto
+                            ">
                             <CardContent>
-                              <div className="flex flex-row text-xl  text-black  mt-4">
-                                <div className="w-full flex flex-col">
+                              <div 
+                                className="
+                                flex flex-row text-xl text-black mt-4
+                                xl:flex xl:flex-col xl:text-xl xl:text-black xl:mt-4
+                                2xl:flex 2xl:flex-row 2xl:text-xl 2xl:text-black 2xl:mt-4
+                                ">
+                                <div 
+                                  className="
+                                  w-full flex flex-col                                         
+                                  sm:w-full sm:flex sm:flex-col
+                                  md:w-full md:flex md:flex-col
+                                  lg:w-full lg:flex lg:flex-col
+                                  xl:w-full xl:flex xl:flex-col xl:items-center
+                                  2xl:w-full 2xl:flex 2xl:flex-col
+                                  ">
                                   <h1
                                     className="
                                   text-lg ml-20 font-semibold mb-12
+                                  xl:text-lg xl:ml-0 xl:font-semibold xl:mb-12
                                   2xl:text-2xl 2xl:font-semibold 2xl:mb-12
                                   "
                                   >
@@ -173,10 +220,10 @@ export default function AssignedTask_Archiving_tabs({
                                 </div>
                                 <div
                                   className="
-                                w-[60%]
-                                2xl:w-[60%] items-center
-                                "
-                                >
+                                  w-[60%]
+                                  2xl:w-[60%] items-center
+                                  "
+                                  >
                                   <div className="flex flex-col mt-48 -ml-40 bg-white p-4 gap-4 drop-shadow-2xl rounded-xl">
                                     <div className="flex flex-row gap-x-48">
                                       <h1 className="text-md">Your work</h1>
@@ -354,8 +401,8 @@ export default function AssignedTask_Archiving_tabs({
           </div>
 
           <div className="flex flex-col bg-white w-[80%] lg:w-[60%] md:w-[60%] sm:[60%] p-4 rounded-md">
-            <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
-              This week:
+            <label className="w-full text-[#5B0505] text-xl text-start font-semibold mr-4">
+              This Week:
             </label>
             <div className="w-full">
               <select
@@ -408,7 +455,7 @@ export default function AssignedTask_Archiving_tabs({
           </div>
 
           <div className="flex flex-col bg-white w-[80%] lg:w-[60%] md:w-[60%] sm:[60%] p-4 rounded-md">
-            <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
+            <label className="w-full text-[#5B0505] text-xl text-start font-semibold mr-4">
               Next week:
             </label>
             <div className="w-full">
@@ -460,7 +507,7 @@ export default function AssignedTask_Archiving_tabs({
           </div>
 
           <div className="flex flex-col bg-white w-[80%] lg:w-[60%] md:w-[60%] sm:[60%] p-4 rounded-md">
-            <label className="w-full text-[#5B0505] text-lg font-semibold mr-4">
+            <label className="w-full text-[#5B0505] text-xl text-start font-semibold mr-4">
               Later:
             </label>
             <div className="w-full">
