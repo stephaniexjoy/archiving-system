ad707fd24dfa9671e649d6b0619122ccc789c258
xcutiepatootie <mackycalabia@gmail.com>
2024-03-19 23:17:33 +0800

    style: login page removed comments

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,10 @@
 import Login from "./components/Login";
-import { compileEmailTemplate, sendMail } from "./lib/mailer/mail";
-import { Button } from "@/components/ui/button";
 
 export default async function Home() {
-  /*  const send = async () => {
-    "use server";
-    await sendMail({
-      to: "matienzonoreenma.patrice@gmail.com",
-      name: "aj",
-      subject: "Forgot Pass",
-      body: `<h1>yabag</h1>`  compileEmailTemplate(
-        "Test",
-        "fb.com"
-      ),
-    });
-  }; */
+  
   return (
     <main className="">
       <section id="home">
-        {/* <form>
-          <Button formAction={send}>test</Button>
-        </form> */}
         <Login />
       </section>
     </main>
