15305e87783b2b3a63d75e4b938deac9dbaf6c88
xcutiepatootie <mackycalabia@gmail.com>
2024-03-20 22:47:13 +0800

    feat: header used in layout instead of pages

--- src/app/dashboard/archiving/page.js
+++ src/app/dashboard/archiving/page.js
@@ -34,7 +34,6 @@ export default async function archiving({ searchParams }) {
   const tasks = await getTasks();
   const completedTasks = await getCompletedTasks();
 
-
   if (searchParams) {
     const { query } = searchParams;
 
@@ -56,7 +55,7 @@ export default async function archiving({ searchParams }) {
   }
 
   const data = await getData();
-  console.log(data)
+  console.log(data);
 
   const dataWithFormattedDate = data.map((file) => ({
     ...file,
@@ -70,28 +69,11 @@ export default async function archiving({ searchParams }) {
     })); */
 
   // const fetchMaterials = await getMaterials()
-   console.log(dataWithFormattedDate)
+  console.log(dataWithFormattedDate);
 
   return (
     <>
       <div className="flex flex-col w-screen h-screen overflow-y-auto">
-      <div className="bg-[#AD5606] w-[100%] h-[12%] flex items-center justify-center">
-        <Image
-          className="
-          w-[60%] h-auto object-cover flex items-center justify-center
-          sm:w-[60%] sm:h-[100%] sm:object-cover sm:flex sm:items-center sm:justify-center
-          md:w-[60%] md:h-[100%] md:object-cover md:flex md:items-center md:justify-center
-          lg:w-[40%] lg:h-[100%] lg:object-cover lg:flex lg:items-center lg:justify-center
-          xl:w-[35%] xl:h-[100%] xl:object-cover xl:flex xl:items-center xl:justify-center
-          2xl:w-[30%] 2xl:h-[100%] 2xl:object-cover 2xl:flex 2xl:items-center 2xl:justify-center
-          "
-          alt="E-Archiving System"
-          src="/photos/E-Archiving System.png"
-          width={2125}
-          height={499}
-        />
-      </div>
-
         <ArchivingTab
           datas={dataWithFormattedDate}
           materials={fetchMaterials}
