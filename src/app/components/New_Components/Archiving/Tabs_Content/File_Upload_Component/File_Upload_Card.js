f1959a91e7242ea3f94725559f0e7ebd154eb847
Asdf123ah <0320-0770@lspu.edu.ph>
2024-03-20 15:21:04 +0800

    edit file upload

--- src/app/components/New_Components/Archiving/Tabs_Content/File_Upload_Component/File_Upload_Card.js
+++ src/app/components/New_Components/Archiving/Tabs_Content/File_Upload_Component/File_Upload_Card.js
@@ -48,7 +48,10 @@ const File_Upload_Card = ({ task }) => {
         <div
           className="
           flex flex-col text-xl text-black mt-4
-          xl:flex xl:flex-col xl:text-xl xl:text-black xl:mt-4
+          sm:flex sm:flex-col sm:text-xl sm:text-black sm:mt-4
+          md:flex md:flex-col md:text-xl md:text-black md:mt-4
+          lg:flex lg:flex-col lg:text-xl lg:text-black lg:mt-4 lg:items-center
+          xl:flex xl:flex-row xl:text-xl xl:text-black xl:mt-4
           2xl:flex 2xl:flex-row 2xl:text-xl 2xl:text-black 2xl:mt-4
           "
         >
@@ -57,7 +60,7 @@ const File_Upload_Card = ({ task }) => {
             w-full flex flex-col                                         
             sm:w-full sm:flex sm:flex-col
             md:w-full md:flex md:flex-col
-            lg:w-full lg:flex lg:flex-col
+            lg:w-full lg:flex lg:flex-col lg:items-center
             xl:w-full xl:flex xl:flex-col xl:items-center
             2xl:w-full 2xl:flex 2xl:flex-col
             "
@@ -92,37 +95,44 @@ const File_Upload_Card = ({ task }) => {
           </div>
           <div
             className="
-            w-full
-            2xl:w-[60%] items-center
+            flex w-full justify-center items-center
+            sm:w-full
+            md:w-full
+            lg:w-full
+            xl:w-full
+            2xl:w-full 
             "
           >
             <div
               className="
               flex flex-col bg-slate-100 p-4 gap-2 shadow-2xl rounded-xl mt-4
-              xl:flex xl:flex-col xl:mt-48 xl:-ml-40 xl:p-4 xl:gap-4 xl:drop-shadow-2xl xl:rounded-xl
-              2xl:flex 2xl:flex-col 2xl:mt-48 2xl:-ml-40 2xl:p-4 2xl:gap-4 2xl:drop-shadow-2xl 2xl:rounded-xl
+              sm:flex sm:flex-col sm:bg-slate-100 sm:p-8 sm:gap-2 sm:shadow-2xl sm:rounded-xl sm:mt-4
+              md:flex md:flex-col md:bg-slate-100 md:p-8 md:gap-2 md:shadow-2xl md:rounded-xl md:mt-4
+              lg:flex lg:flex-col lg:bg-slate-100 lg:p-8 lg:gap-2 lg:shadow-2xl lg:rounded-xl lg:mt-4
+              xl:flex xl:flex-col xl:mt-0 xl:-ml-40 xl:p-8 xl:gap-4 xl:drop-shadow-2xl xl:rounded-xl
+              2xl:flex 2xl:flex-col 2xl:mt-0 2xl:ml-0 2xl:p-4 2xl:gap-0 2xl:drop-shadow-2xl 2xl:rounded-xl
               "
             >
               <div
                 className="
                 flex flex-row gap-x-32
-                sm:flex sm:flex-row sm:gap-x-0
-                md:flex md:flex-row md:gap-x-0
-                lg:flex lg:flex-row lg:gap-x-0
+                sm:flex sm:flex-row sm:gap-x-64
+                md:flex md:flex-row md:gap-x-64 md:mb-5
+                lg:flex lg:flex-row lg:gap-x-72 lg:mb-5
                 xl:flex xl:flex-row xl:gap-x-0
-                2xl:flex 2xl:flex-row 2xl:gap-x-0
+                2xl:flex 2xl:flex-row 2xl:gap-x-0 w-auto
                 "
               >
-                <h1 className="text-sm sm:text-md md:text-md lg:text-md xl:text-md 2xl:text-md">
+                <h1 className="text-sm sm:text-md md:text-lg lg:text-md xl:text-md 2xl:text-md">
                   Your work
                 </h1>
-                <h1 className="text-sm -ml-5 text-green-600">Assigned</h1>
+                <h1 className="text-sm sm:text-sm md:text-lg -ml-5 text-green-600">Assigned</h1>
               </div>
               <Dialog>
-                <DialogTrigger className="flex mx-auto w-[80%] h-10 border bg-[#AD5606] hover:bg-gray-100 text-white font-bold rounded cursor-pointer items-center justify-center mt-8 sm:w-full">
+                <DialogTrigger className="flex mx-auto w-[80%] sm:w-[70%] h-10 border bg-[#AD5606] hover:bg-gray-100 text-white font-bold rounded cursor-pointer items-center justify-center mt-8">
                   Upload Here
                 </DialogTrigger>
-                <DialogContent className="bg-slate-50 w-[90%] h-auto py-6 px-6 mx-auto overflow-y-auto">
+                <DialogContent className="bg-slate-50 w-[90%] sm:w-[80%] h-auto py-6 px-6 mx-auto overflow-y-auto">
                   <DialogHeader>
                     <DialogTitle className="text-2xl">Upload files</DialogTitle>
                     <DialogDescription>
@@ -212,7 +222,7 @@ const File_Upload_Card = ({ task }) => {
                   console.log(res);
                   console.log(fileInfo);
                 }}
-                className="flex mx-auto w-[80%] h-10 border bg-[#AD5606] hover:bg-gray-100 text-white font-bold py-1 px-4 rounded my-2 cursor-pointer items-center justify-center sm:w-full"
+                className="flex mx-auto w-[80%] sm:w-[70%] h-10 border bg-[#AD5606] hover:bg-gray-100 text-white font-bold py-1 px-4 rounded my-2 cursor-pointer items-center justify-center"
               >
                 Mark as done
               </button>
