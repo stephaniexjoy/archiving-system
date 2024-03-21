0c265d273f7137ac3e4205d8fb844aa7d1f9c8d1
beejayreyes <beejay.reyes0112@gmail.com>
2024-03-21 13:33:29 +0800

    refactor: loading js of secretary view faculty

--- src/app/secretary/dashboard/viewfaculty/loading.js
+++ src/app/secretary/dashboard/viewfaculty/loading.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { Skeleton } from '@/components/ui/skeleton'
-import ViewFaculty_Secretary from '@/app/components/NewTable/ViewFaculty_Table_Secretary/ViewFaculty_Secretary'
 import ViewFaculty_Skeleton from '@/app/components/Skeletons/Secretary/Dashboard/View Faculty/ViewFaculty_Skeleton'
 
 export default function loading() {
