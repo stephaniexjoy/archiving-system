59b54ed501b8897acefed58ddb4f10675a0d3f7f
beejayreyes <beejay.reyes0112@gmail.com>
2024-03-21 15:16:22 +0800

    feat: tasks skeleton

---
+++ src/app/dashboard/archiving/task/[[...taskId]]/loading.js
@@ -0,0 +1,8 @@
+import Tasks_Skeleton from '@/app/components/Skeletons/Faculty/Dashboard/Archiving/Tasks/Tasks_Skeleton'
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div><Tasks_Skeleton /></div>
+  )
+}
