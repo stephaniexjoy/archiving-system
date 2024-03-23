cdb021849d0b3d75d85be2d89e4a5b898ebc82ce
xcutiepatootie <mackycalabia@gmail.com>
2024-03-23 16:10:49 +0800

    feat: secretary backbutton

--- src/app/secretary/dashboard/viewfaculty/view-user/[id]/page.js
+++ src/app/secretary/dashboard/viewfaculty/view-user/[id]/page.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { db } from "@/app/lib/prisma_db";
+import BackButton from "@/app/components/New_Components/Buttons/Secretary/BackButton";
 
 const page = async ({ params }) => {
   const user = await db.user.findUnique({
@@ -345,35 +346,9 @@ const page = async ({ params }) => {
             "
           >
             <div className="flex items-center">
-              <button
-                type="submit"
-                className="
-                bg-[#5B0505] text-[15px] text-white text-center w-[100px] h-[30px]
-                sm:bg-[#5B0505] sm:text-[17px] sm:text-white sm:text-center sm:w-[130px] sm:h-[32px]
-                md:bg-[#5B0505] md:text-[19px] md:text-white md:text-center md:w-[130px] md:h-[34px]
-                lg:bg-[#5B0505] lg:text-[21px] lg:text-white lg:text-centerlg: lg:w-[180px] lg:h-[36px]
-                xl:bg-[#5B0505] xl:text-[23px] xl:text-white xl:text-center xl:w-[220px] xl:h-[38px]
-                2xl:bg-[#5B0505] 2xl:text-[25px] 2xl:text-white 2xl:text-center 2xl:w-[250px] 2xl:h-[40px]
-                "
-              >
-                BACK
-              </button>
+              <BackButton />
             </div>
-            <div className="flex items-center">
-              <button
-                type="submit"
-                className="
-                bg-[#5B0505] text-[15px] text-white text-center w-[100px] h-[30px]
-                sm:bg-[#5B0505] sm:text-[17px] sm:text-white sm:text-center sm:w-[130px] sm:h-[32px]
-                md:bg-[#5B0505] md:text-[19px] md:text-white md:text-center md:w-[130px] md:h-[34px]
-                lg:bg-[#5B0505] lg:text-[21px] lg:text-white lg:text-center lg:w-[180px] lg:h-[36px]
-                xl:bg-[#5B0505] xl:text-[23px] xl:text-white xl:text-center xl:w-[220px] xl:h-[38px]
-                2xl:bg-[#5B0505] 2xl:text-[25px] 2xl:text-white 2xl:text-center 2xl:w-[250px] 2xl:h-[40px]
-                "
-              >
-                DOWNLOAD
-              </button>
-            </div>
+            <div className="flex items-center"></div>
             <div className="flex items-center">
               <button
                 type="submit"
