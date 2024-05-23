df006fd707bf5de87c9a242bfd5d3292785e8311
beejayreyes <beejay.reyes0112@gmail.com>
2024-05-23 16:11:28 +0800

    refactor: sec archiving skeleton

--- src/app/dashboard/archiving/loading.js
+++ src/app/dashboard/archiving/loading.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import Archiving_Skeleton from '@/app/components/Skeletons/Faculty/Dashboard/Archiving/Archiving_Skeleton'
+import Faculty_Archiving_Skeleton from '@/app/components/Skeletons/Faculty/Dashboard/Archiving/Archiving_Skeleton'
+import Secretary_Archiving_Skeleton from '@/app/components/Skeletons/Secretary/Dashboard/Archiving/Archiving_Skeleton'
 
-export default function loading() {
-  return (
-    <div><Archiving_Skeleton /></div>
-  )
+import { getServerSession } from "next-auth";
+import { AuthOptions } from '@/app/api/auth/[...nextauth]/options';
+
+export default async function loading() {
+  const session = await getServerSession(AuthOptions);
+
+  if (session.user.position === "Secretary") {
+    return <Secretary_Archiving_Skeleton />;
+  }
+
+  return <Faculty_Archiving_Skeleton />;
 }
