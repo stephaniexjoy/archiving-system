25271c6f254c2a57c1fcb8e3287a96cee757a31d
xcutiepatootie <mackycalabia@gmail.com>
2024-05-23 11:18:48 +0800

    fix: skeletons

--- src/app/dashboard/profile/loading.js
+++ src/app/dashboard/profile/loading.js
@@ -1,8 +1,18 @@
-import Profile_Skeleton from '@/app/components/Skeletons/Faculty/Dashboard/Profile/Profile_Skeleton'
-import React from 'react'
+import { AuthOptions } from "@/app/api/auth/[...nextauth]/options";
+import Profile_Skeleton from "@/app/components/Skeletons/Faculty/Dashboard/Profile/Profile_Skeleton";
+import Secretary_Profile_Skeleton from "@/app/components/Skeletons/Secretary/Dashboard/Profile/Profile_Skeleton";
+import { getServerSession } from "next-auth";
+import React from "react";
 
-export default function loading() {
+export default async function loading() {
+  const session = await getServerSession(AuthOptions);
+
+  if (session.user.position === "Secretary") {
+    return <Secretary_Dashboard_Skeleton />;
+  }
   return (
-    <div><Profile_Skeleton/></div>
-  )
+    <div>
+      <Profile_Skeleton />
+    </div>
+  );
 }
